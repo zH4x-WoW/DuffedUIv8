246f1cb3475046b3581ae08f0accfe7b96f84eec
liquidbase <liquid.base.x@googlemail.com>
2013-08-16 20:00:55 +0200

    blah

--- DuffedUI/Modules/Tooltips/IdAndCount.lua
+++
@@ -1,5 +0,0 @@
-local D, C, L = select(2, ...):unpack()
-
-if not C["tooltips"].Enable then
-	return
-end
--- DuffedUI/Modules/UnitFrames/Classes/Hunter.lua
+++
@@ -1,8 +0,0 @@
-local D, C, L = select(2, ...):unpack()
-
-local DuffedUIUnitFrames = D["UnitFrames"]
-local Class = select(2, UnitClass("player"))
-
-function DuffedUIUnitFrames:AddHunterFeatures()
-
-end
