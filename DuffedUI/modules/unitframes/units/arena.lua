6d7f719caccde0584d7b723b75ecd87af089ba38
Merith <liquid.base.x@googlemail.com>
2014-06-05 18:13:16 +0200

    Boss & Arenaframe finished

--- DuffedUI/modules/unitframes/units/arena.lua
+++ DuffedUI/modules/unitframes/units/arena.lua
@@ -6,9 +6,6 @@ function DuffedUIUnitFrames:Arena()
 	self:RegisterForClicks("AnyUp")
 	self:SetScript("OnEnter", UnitFrame_OnEnter)
 	self:SetScript("OnLeave", UnitFrame_OnLeave)
-	self:SetBackdrop(DuffedUIUnitFrames.Backdrop)
-	self:SetBackdropColor(0, 0, 0)
-	self:CreateShadow()
 	self:SetAttribute("type2", "focus")
 	
 	local Health = CreateFrame("StatusBar", nil, self)
@@ -19,48 +16,66 @@ function DuffedUIUnitFrames:Arena()
 	Health.Background = Health:CreateTexture(nil, "BORDER")
 	Health.Background:SetAllPoints()
 	Health.Background:SetTexture(.1, .1, .1)
-	Health:FontString("Value", C["medias"].AltFont, 12, "OUTLINE")
+	local HealthBorder = CreateFrame("Frame", nil, Health)
+	HealthBorder:SetPoint("TOPLEFT", Health, "TOPLEFT", D.Scale(-2), D.Scale(2))
+	HealthBorder:SetPoint("BOTTOMRIGHT", Health, "BOTTOMRIGHT", D.Scale(2), D.Scale(-2))
+	HealthBorder:SetTemplate("Default")
+	HealthBorder:CreateShadow("Default")
+	HealthBorder:SetFrameLevel(2)
+	Health:FontString("Value", C["medias"].Font, 12, "OUTLINE")
 	Health.Value:Point("LEFT", 2, 0)
 	Health.PostUpdate = DuffedUIUnitFrames.PostUpdateHealth
 	Health.frequentUpdates = true
-	Health.colorClass = true
-	Health.colorDisconnected = true
-	Health.colorReaction = true
-	if (C["unitframes"].Smooth) then
-		Health.Smooth = true
+	if C["unitframes"].UniColor == true then
+		Health.colorDisconnected = false
+		Health.colorClass = false
+		Health.colorReaction = false
+		Health:SetStatusBarColor(unpack(C["unitframes"].HealthBarColor))
+		Health.Background:SetVertexColor(unpack(C["unitframes"].HealthBGColor))
+	else
+		Health.colorDisconnected = true
+		Health.colorClass = true
+		Health.colorReaction = true
+		Health.Background:SetTexture(.1, .1, .1)
 	end
+	if (C["unitframes"].Smooth) then Health.Smooth = true end
 
 	-- Power
 	local Power = CreateFrame("StatusBar", nil, self)
-	Power:Height(6)
-	Power:Point("TOPLEFT", Health, "BOTTOMLEFT", 0, -1)
-	Power:Point("TOPRIGHT", Health, "BOTTOMRIGHT", 0, -1)
+	Power:Height(3)
+	Power:Point("TOPLEFT", Health, "BOTTOMLEFT", 96, 0)
+	Power:Point("TOPRIGHT", Health, "BOTTOMRIGHT", -10, 0)
 	Power.Background = Power:CreateTexture(nil, "BORDER")
 	Power.Background:SetAllPoints(Power)
 	Power.Background:SetTexture(C["medias"].Normal)
 	Power.Background.multiplier = 0.3
 	Power:SetStatusBarTexture(C["medias"].Normal)
-	Power:FontString("Value", C["medias"].AltFont, 12, "OUTLINE")
+	Power:SetFrameLevel(Health:GetFrameLevel() + 1)
+	local PowerBorder = CreateFrame("Frame", nil, Power)
+	PowerBorder:SetPoint("TOPLEFT", Power, "TOPLEFT", D.Scale(-2), D.Scale(2))
+	PowerBorder:SetPoint("BOTTOMRIGHT", Power, "BOTTOMRIGHT", D.Scale(2), D.Scale(-2))
+	PowerBorder:SetTemplate("Default")
+	PowerBorder:CreateShadow("Default")
+	PowerBorder:SetFrameLevel(Health:GetFrameLevel() + 1)
+	Power:FontString("Value", C["medias"].Font, 12, "OUTLINE")
 	Power.Value:Point("RIGHT", -2, 0)
 	Power.PostUpdate = DuffedUIUnitFrames.PostUpdatePower
 	Power.frequentUpdates = true
 	Power.colorPower = true
-	if (C["unitframes"].Smooth) then
-		Health.Smooth = true
-	end
+	if (C["unitframes"].Smooth) then Health.Smooth = true end
 			
 	local Name = Health:CreateFontString(nil, "OVERLAY")
 	Name:SetPoint("CENTER", Health, "CENTER", 0, 0)
 	Name:SetJustifyH("CENTER")
-	Name:SetFont(C["medias"].AltFont, 12, "OUTLINE")
+	Name:SetFont(C["medias"].Font, 12, "OUTLINE")
 	Name:SetShadowColor(0, 0, 0)
 	Name:SetShadowOffset(1.25, -1.25)
 	Name.frequentUpdates = 0.2
 
 	local Debuffs = CreateFrame("Frame", nil, self)
-	Debuffs:SetHeight(26)
+	Debuffs:SetHeight(22)
 	Debuffs:SetWidth(200)
-	Debuffs:Point("LEFT", self, "RIGHT", 4, 0)
+	Debuffs:Point("LEFT", self, "RIGHT", 4, 3)
 	Debuffs.size = 26
 	Debuffs.num = 5
 	Debuffs.spacing = 2
@@ -71,71 +86,76 @@ function DuffedUIUnitFrames:Arena()
 			
 	local SpecIcon = CreateFrame("Frame", nil, self)
 	SpecIcon:Size(22)
-	SpecIcon:SetPoint("RIGHT", self, "LEFT", -6, 0)
+	SpecIcon:SetPoint("RIGHT", self, "LEFT", -6, 3)
 	SpecIcon:CreateBackdrop()
 	SpecIcon.Backdrop:CreateShadow()
 
 	local Trinket = CreateFrame("Frame", nil, self)
 	Trinket:Size(22)
-	Trinket:SetPoint("RIGHT", self, "LEFT", -34, 0)
+	Trinket:SetPoint("RIGHT", self, "LEFT", -34, 3)
 	Trinket:CreateBackdrop()
 	Trinket.Backdrop:CreateShadow()
 	
-	if (C["unitframes"].CastBar) then
-		local CastBar = CreateFrame("StatusBar", nil, self)
-		CastBar:SetPoint("LEFT", 24, 0)
-		CastBar:SetPoint("RIGHT", -2, 0)
-		CastBar:SetPoint("BOTTOM", 0, -22)
-		CastBar:SetHeight(16)
-		CastBar:SetStatusBarTexture(C["medias"].Normal)
-		CastBar:SetFrameLevel(6)
-		CastBar.Background = CreateFrame("Frame", nil, CastBar)
-		CastBar.Background:SetTemplate("Default")
-		CastBar.Background:SetBackdropBorderColor(C["medias"].BorderColor[1] * 0.7, C["medias"].BorderColor[2] * 0.7, C["medias"].BorderColor[3] * 0.7)
-		CastBar.Background:Point("TOPLEFT", -2, 2)
-		CastBar.Background:Point("BOTTOMRIGHT", 2, -2)
-		CastBar.Background:SetFrameLevel(5)
-		
-		CastBar.Time = CastBar:CreateFontString(nil, "OVERLAY")
-		CastBar.Time:SetFont(C["medias"].AltFont, 12)
-		CastBar.Time:Point("RIGHT", CastBar, "RIGHT", -4, 0)
-		CastBar.Time:SetTextColor(0.84, 0.75, 0.65)
-		CastBar.Time:SetJustifyH("RIGHT")
-		CastBar.CustomTimeText = DuffedUIUnitFrames.CustomCastTimeText
+	local CastBar = CreateFrame("StatusBar", nil, self)
+	CastBar:SetPoint("LEFT", 24, 0)
+	CastBar:SetPoint("RIGHT", -2, 0)
+	CastBar:SetPoint("BOTTOM", 0, -22)
+	CastBar:SetHeight(10)
+	CastBar:SetStatusBarTexture(C["medias"].Normal)
+	CastBar:SetFrameLevel(6)
+	CastBar.Background = CreateFrame("Frame", nil, CastBar)
+	CastBar.Background:SetTemplate("Default")
+	CastBar.Background:SetBackdropBorderColor(C["medias"].BorderColor[1] * 0.7, C["medias"].BorderColor[2] * 0.7, C["medias"].BorderColor[3] * 0.7)
+	CastBar.Background:Point("TOPLEFT", -2, 2)
+	CastBar.Background:Point("BOTTOMRIGHT", 2, -2)
+	CastBar.Background:SetFrameLevel(5)
+	local CastBarBorder = CreateFrame("Frame", nil, CastBar)
+	CastBarBorder:SetPoint("TOPLEFT", CastBar, "TOPLEFT", D.Scale(-2), D.Scale(2))
+	CastBarBorder:SetPoint("BOTTOMRIGHT", CastBar, "BOTTOMRIGHT", D.Scale(2), D.Scale(-2))
+	CastBarBorder:SetTemplate("Default")
+	CastBarBorder:CreateShadow("Default")
+	
+	CastBar.Time = CastBar:CreateFontString(nil, "OVERLAY")
+	CastBar.Time:SetFont(C["medias"].Font, 12)
+	CastBar.Time:Point("RIGHT", CastBar, "RIGHT", -4, 0)
+	CastBar.Time:SetTextColor(0.84, 0.75, 0.65)
+	CastBar.Time:SetJustifyH("RIGHT")
+	CastBar.CustomTimeText = DuffedUIUnitFrames.CustomCastTimeText
 
-		CastBar.Text = CastBar:CreateFontString(nil, "OVERLAY")
-		CastBar.Text:SetFont(C["medias"].AltFont, 12)
-		CastBar.Text:Point("LEFT", CastBar, "LEFT", 4, 0)
-		CastBar.Text:SetTextColor(0.84, 0.75, 0.65)
-		
-		CastBar.CustomDelayText = DuffedUIUnitFrames.CustomCastDelayText
-		CastBar.PostCastStart = DuffedUIUnitFrames.CheckCast
-		CastBar.PostChannelStart = DuffedUIUnitFrames.CheckChannel
-								
-		CastBar.Button = CreateFrame("Frame", nil, CastBar)
-		CastBar.Button:Height(CastBar:GetHeight() + 4)
-		CastBar.Button:Width(CastBar:GetHeight() + 4)
-		CastBar.Button:Point("RIGHT", CastBar, "LEFT", -4, 0)
-		CastBar.Button:SetTemplate()
-		CastBar.Button:SetBackdropBorderColor(C["medias"].BorderColor[1] * 0.7, C["medias"].BorderColor[2] * 0.7, C["medias"].BorderColor[3] * 0.7)
-		
-		CastBar.Icon = CastBar.Button:CreateTexture(nil, "ARTWORK")
-		CastBar.Icon:Point("TOPLEFT", CastBar.Button, 2, -2)
-		CastBar.Icon:Point("BOTTOMRIGHT", CastBar.Button, -2, 2)
-		CastBar.Icon:SetTexCoord(unpack(D.IconCoord))
-
-		self.Castbar = CastBar
-		self.Castbar.Time = CastBar.Time
-		self.Castbar.Icon = CastBar.Icon
-	end
+	CastBar.Text = CastBar:CreateFontString(nil, "OVERLAY")
+	CastBar.Text:SetFont(C["medias"].Font, 12)
+	CastBar.Text:Point("LEFT", CastBar, "LEFT", 4, 0)
+	CastBar.Text:SetTextColor(0.84, 0.75, 0.65)
+	
+	CastBar.CustomDelayText = DuffedUIUnitFrames.CustomCastDelayText
+	CastBar.PostCastStart = DuffedUIUnitFrames.CheckCast
+	CastBar.PostChannelStart = DuffedUIUnitFrames.CheckChannel
+							
+	CastBar.Button = CreateFrame("Frame", nil, CastBar)
+	CastBar.Button:Height(CastBar:GetHeight() + 4)
+	CastBar.Button:Width(CastBar:GetHeight() + 4)
+	CastBar.Button:Point("RIGHT", CastBar, "LEFT", -4, 0)
+	CastBar.Button:SetTemplate()
+	CastBar.Button:SetBackdropBorderColor(C["medias"].BorderColor[1] * 0.7, C["medias"].BorderColor[2] * 0.7, C["medias"].BorderColor[3] * 0.7)
+	
+	CastBar.Icon = CastBar.Button:CreateTexture(nil, "ARTWORK")
+	CastBar.Icon:Point("TOPLEFT", CastBar.Button, 2, -2)
+	CastBar.Icon:Point("BOTTOMRIGHT", CastBar.Button, -2, 2)
+	CastBar.Icon:SetTexCoord(unpack(D.IconCoord))
 	
 	self:Tag(Name, "[DuffedUI:GetNameColor][DuffedUI:NameLong]")
 	self.Health = Health
 	self.Health.bg = Health.Background
+	self.HealthBorder = HealthBorder
 	self.Power = Power
 	self.Power.bg = Power.Background
+	self.PowerBorder = PowerBorder
 	self.Name = Name
 	self.Debuffs = Debuffs
 	self.PVPSpecIcon = SpecIcon
 	self.Trinket = Trinket
+	self.Castbar = CastBar
+	self.Castbar.Time = CastBar.Time
+	self.Castbar.Icon = CastBar.Icon
+	self.CastBarBorder = CastBarBorder
 end
