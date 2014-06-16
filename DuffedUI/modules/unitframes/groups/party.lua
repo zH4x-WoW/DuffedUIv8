ac9210f9ff1b1e1f6a493aa0ed2414f6f0e79e04
Merith <liquid.base.x@googlemail.com>
2014-06-16 16:58:15 +0200

    Partyframe finished

--- DuffedUI/modules/unitframes/groups/party.lua
+++ DuffedUI/modules/unitframes/groups/party.lua
@@ -7,66 +7,96 @@ function DuffedUIUnitFrames:Party()
 	self:RegisterForClicks("AnyUp")
 	self:SetScript("OnEnter", UnitFrame_OnEnter)
 	self:SetScript("OnLeave", UnitFrame_OnLeave)
-	self:SetBackdrop(DuffedUIUnitFrames.Backdrop)
-	self:SetBackdropColor(0, 0, 0)
-	self:CreateShadow()
 	
 	local Health = CreateFrame("StatusBar", nil, self)
 	Health:SetPoint("TOPLEFT")
 	Health:SetPoint("TOPRIGHT")
 	Health:Height(self:GetHeight() - 5)
 	Health:SetStatusBarTexture(C["medias"].Normal)
+
 	Health.Background = Health:CreateTexture(nil, "BORDER")
 	Health.Background:SetAllPoints()
 	Health.Background:SetTexture(.1, .1, .1)
+
+	-- Border for HealthBar
+	local HealthBorder = CreateFrame("Frame", nil, Health)
+	HealthBorder:SetPoint("TOPLEFT", Health, "TOPLEFT", D.Scale(-2), D.Scale(2))
+	HealthBorder:SetPoint("BOTTOMRIGHT", Health, "BOTTOMRIGHT", D.Scale(2), D.Scale(-2))
+	HealthBorder:SetTemplate("Default")
+	HealthBorder:CreateShadow("Default")
+	HealthBorder:SetFrameLevel(2)
+
 	Health.frequentUpdates = true
-	Health.colorClass = true
-	Health.colorDisconnected = true
-	Health.colorReaction = true
-	if (C["unitframes"].Smooth) then
-		Health.Smooth = true
+	if C["unitframes"].UniColor == true then
+		Health.colorDisconnected = false
+		Health.colorClass = false
+		Health.colorReaction = false
+		Health:SetStatusBarColor(unpack(C["unitframes"].HealthBarColor))
+		Health.Background:SetVertexColor(unpack(C["unitframes"].HealthBGColor))
+	else
+		Health.colorDisconnected = true
+		Health.colorClass = true
+		Health.colorReaction = true
+		Health.Background:SetTexture(.1, .1, .1)
 	end
+	if (C["unitframes"].Smooth) then Health.Smooth = true end
 	
 	-- Power
 	local Power = CreateFrame("StatusBar", nil, self)
-	Power:Height(4)
-	Power:Point("TOPLEFT", Health, "BOTTOMLEFT", 0, -1)
-	Power:Point("TOPRIGHT", Health, "BOTTOMRIGHT", 0, -1)
+	Power:Height(3)
+	Power:Point("TOPLEFT", Health, "BOTTOMLEFT", 75, 0)
+	Power:Point("TOPRIGHT", Health, "BOTTOMRIGHT", -9, -3)
+	Power:SetStatusBarTexture(C["medias"].Normal)
+	Power:SetFrameLevel(Health:GetFrameLevel() + 1)
+
 	Power.Background = Power:CreateTexture(nil, "BORDER")
 	Power.Background:SetAllPoints(Power)
 	Power.Background:SetTexture(C["medias"].Normal)
 	Power.Background.multiplier = 0.3
-	Power:SetStatusBarTexture(C["medias"].Normal)
+
+	-- Border for Power
+	local PowerBorder = CreateFrame("Frame", nil, Power)
+	PowerBorder:SetPoint("TOPLEFT", Power, "TOPLEFT", D.Scale(-2), D.Scale(2))
+	PowerBorder:SetPoint("BOTTOMRIGHT", Power, "BOTTOMRIGHT", D.Scale(2), D.Scale(-2))
+	PowerBorder:SetTemplate("Default")
+	PowerBorder:CreateShadow("Default")
+	PowerBorder:SetFrameLevel(Health:GetFrameLevel() + 1)
+	
 	Power.frequentUpdates = true
 	Power.colorPower = true
-	if (C["unitframes"].Smooth) then
-		Health.Smooth = true
-	end
+	if (C["unitframes"].Smooth) then Health.Smooth = true end
 	
 	local Name = Health:CreateFontString(nil, "OVERLAY")
 	Name:SetPoint("TOPLEFT", -1, 18)
-	Name:SetFont(C["medias"].AltFont, 14, "THINOUTLINE")
+	Name:SetFont(C["medias"].Font, 14, "THINOUTLINE")
 	
 	local Role = Health:CreateFontString(nil, "OVERLAY")
 	Role:SetPoint("TOPRIGHT", 3, 18)
-	Role:SetFont(C["medias"].AltFont, 14, "THINOUTLINE")
+	Role:SetFont(C["medias"].Font, 14, "THINOUTLINE")
 	
 	if (C["party"].Portrait) then
 		local Portrait = CreateFrame("Frame", nil, self)
-		Portrait:Size(40)
-		Portrait:SetPoint("BOTTOMRIGHT", self, "BOTTOMLEFT", -4,0)
+		Portrait:Size(35)
+		Portrait:SetPoint("BOTTOMRIGHT", Health, "BOTTOMLEFT", -6, 0)
 		Portrait:SetBackdrop(DuffedUIUnitFrames.Backdrop)
 		Portrait:SetBackdropColor(0, 0, 0)
-		Portrait:CreateShadow()
 		
 		Portrait.Model = CreateFrame("PlayerModel", nil, Portrait)
 		Portrait.Model:SetInside(Portrait, 1, 1)
+
+		-- Border for Portrait
+		local PortraitBorder = CreateFrame("Frame", nil, Portrait)
+		PortraitBorder:SetPoint("TOPLEFT", Portrait, "TOPLEFT", D.Scale(-2), D.Scale(2))
+		PortraitBorder:SetPoint("BOTTOMRIGHT", Portrait, "BOTTOMRIGHT", D.Scale(2), D.Scale(-2))
+		PortraitBorder:SetTemplate("Default")
+		PortraitBorder:CreateShadow("Default")
+		PortraitBorder:SetFrameLevel(2)
 		
 		self.Portrait = Portrait.Model
 	end
 	
 	local Buffs = CreateFrame("Frame", nil, self)
-	Buffs:Point("TOPLEFT", C["party"].Portrait and self.Portrait:GetParent() or self, "BOTTOMLEFT", 0, -6)
+	Buffs:Point("TOPLEFT", C["party"].Portrait and self.Portrait:GetParent() or self, "BOTTOMLEFT", -2, -5)
 	Buffs:SetHeight(24)
 	Buffs:SetWidth(250)
 	Buffs.size = 24
@@ -102,8 +132,10 @@ function DuffedUIUnitFrames:Party()
 	
 	self.Health = Health
 	self.Health.bg = Health.Background
+	self.HealthBorder = HealthBorder
 	self.Power = Power
 	self.Power.bg = Power.Background
+	self.PowerBorder = PowerBorder
 	self.Name = Name
 	self.Buffs = Buffs
 	self.Debuffs = Debuffs
