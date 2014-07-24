ad802873d0aeb1cdc85abf42b47b888096c03b6e
liquidbase <liquid.base.x@googlemail.com>
2014-07-24 14:13:01 +0200

    Warriorstuff

--- DuffedUI/modules/unitframes/classes/warrior.lua
+++ DuffedUI/modules/unitframes/classes/warrior.lua
@@ -6,6 +6,58 @@ local Class = select(2, UnitClass("player"))
 if (Class ~= "WARRIOR") then return end
 
 function DuffedUIUnitFrames:AddWarriorFeatures()
+	local Texture = C["medias"].Normal
+	local Font = C["medias"].Font
+	local Color = RAID_CLASS_COLORS[Class]
+	
+	-- Energy Bar
+	local EnergyBarBG = CreateFrame("Frame", "EnergyBarBG", self)
+	EnergyBarBG:SetPoint("TOP", AnchorFrameRessources, "BOTTOM", 0, 0)
+	EnergyBarBG:Size(200, 6)
+	EnergyBarBG:CreateBackdrop()
+
+	local EnergyBar = CreateFrame("StatusBar", nil, EnergyBarBG)
+	EnergyBar:SetStatusBarTexture(Texture)
+	EnergyBar:SetStatusBarColor(Color.r, Color.g, Color.b)
+	EnergyBar:SetMinMaxValues(0, 100)
+	EnergyBar:SetPoint("LEFT", EnergyBarBG, "LEFT", 0, 0)
+	EnergyBar:Size(200, 6)
+
+	EnergyBar.text = EnergyBar:CreateFontString(nil, "ARTWORK")
+	EnergyBar.text:SetFont(Font, 16, "THINOUTLINE")
+	EnergyBar.text:SetPoint("LEFT", EnergyBarBG, "RIGHT", 3, 0)
+	EnergyBar.text:SetTextColor(Color.r, Color.g, Color.b)
+
+	EnergyBar.TimeSinceLastUpdate = 0
+	EnergyBar:SetScript("OnUpdate", function(self, elapsed)
+		self.TimeSinceLastUpdate = self.TimeSinceLastUpdate + elapsed 
+
+		if self.TimeSinceLastUpdate > 0.07 then
+			self:SetMinMaxValues(0, UnitPowerMax("player"))
+			local power = UnitPower("player")
+			self:SetValue(power)
+			if self.text then
+				self.text:SetText(D.ShortValue(power))
+			end
+			self.TimeSinceLastUpdate = 0
+		end
+	end)
+	
+	EnergyBarBG:RegisterEvent("PLAYER_REGEN_DISABLED")
+	EnergyBarBG:RegisterEvent("PLAYER_REGEN_ENABLED")
+	EnergyBarBG:RegisterEvent("PLAYER_ENTERING_WORLD")
+	EnergyBarBG:SetScript("OnEvent", function(self, event)
+		if event == "PLAYER_REGEN_DISABLED" then
+			UIFrameFadeIn(self, (0.3 * (1 - self:GetAlpha())), self:GetAlpha(), 1)
+		elseif event == "PLAYER_REGEN_ENABLED" then
+			UIFrameFadeOut(self, (0.3 * (0 + self:GetAlpha())), self:GetAlpha(), 0)
+		elseif event == "PLAYER_ENTERING_WORLD" then
+			if not InCombatLockdown() then
+				EnergyBarBG:SetAlpha(0)
+			end
+		end
+	end)
+	
 	-- Totem Bar (Demoralizing / Mocking / Skull Banner)
 	local bar = CreateFrame("StatusBar", "DuffedUIStatueBar", self)
 	bar:SetWidth(5)
@@ -25,4 +77,5 @@ function DuffedUIUnitFrames:AddWarriorFeatures()
 
 	-- Register
 	self.Statue = bar
+	self.EnergyBar = EnergyBar
 end
