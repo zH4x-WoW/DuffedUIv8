5f6bd619931d49cc8df54cff0f0df61fdd2810f1
liquidbase <liquid.base.x@googlemail.com>
2014-08-30 18:27:24 +0200

    Fixes

--- DuffedUI/modules/skins/garrison.lua
+++ DuffedUI/modules/skins/garrison.lua
@@ -1,19 +1,6 @@
 local D, C, L = unpack(select(2, ...))
 
 local function LoadGarrisonSkin()
-	-- Landing Page
-	GarrisonLandingPage:StripTextures()
-	GarrisonLandingPage:SetTemplate("Transparent")
-
-	for i = 1, 2 do _G["GarrisonLandingPageTab" .. i]:SkinTab() end
-
-	GarrisonLandingPageTab1:ClearAllPoints()
-	GarrisonLandingPageTab1:Point("BOTTOMLEFT", GarrisonLandingPage, 100, -GarrisonLandingPageTab1:GetHeight())
-	GarrisonLandingPageTab2:ClearAllPoints()
-	GarrisonLandingPageTab2:Point("LEFT", GarrisonLandingPageTab1, "RIGHT", 0, 0)
-	GarrisonLandingPage.CloseButton:SkinCloseButton()
-	GarrisonLandingPageReportListListScrollFrameScrollBar:SkinScrollBar()
-
 	-- Work Orders
 	GarrisonCapacitiveDisplayFrame:StripTextures()
 	GarrisonCapacitiveDisplayFrame:SetTemplate("Transparent")
