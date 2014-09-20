a5905206091aff853c6094576cd35ad75d312ad4
liquidbase <liquid.base.x@googlemail.com>
2014-09-20 10:26:23 +0200

    Fixed UIscaling & AutoScale

--- DuffedUI/core/video.lua
+++ DuffedUI/core/video.lua
@@ -1,16 +1,35 @@
 local D, C, L = unpack(select(2, ...))
 
-D.UIScale = function()
-	if C["general"].autoscale == true then
-		C["general"].uiscale = min(2, max(0.64, 768 / string.match(GetCVar("gxResolution"), "%d+x(%d+)")))
-	end
+if C["general"].autoscale then C["general"].uiscale = min(2, max(0.32, 768 / string.match(D.Resolution, "%d+x(%d+)"))) end
+
+local function NeedReloadUI()
+	local ResolutionDropDown = Display_ResolutionDropDown
+	local X, Y = ResolutionDropDown:getValues()
+	local OldRatio = D.ScreenWidth / D.ScreenHeight
+	local NewRatio = X / Y
+	local OldReso = D.Resolution
+	local NewReso = X.."x"..Y
+
+	if (OldRatio == NewRatio) and (OldReso ~= NewReso) then ReloadUI() end
 end
-D.UIScale()
+
+local Graphic = CreateFrame("Frame")
+Graphic:RegisterEvent("PLAYER_ENTERING_WORLD")
+Graphic:SetScript("OnEvent", function(self, event)
+	local Useuiscale = GetCVar("useUiScale")
+
+	if (Useuiscale ~= "1") then SetCVar("useUiScale", 1) end
+	if (format("%.2f", GetCVar("uiScale")) ~= format("%.2f", C["general"].uiscale)) then SetCVar("uiScale", C["general"].uiscale) end
+	if (C["general"].uiscale < 0.64) then UIParent:SetScale(C["general"].uiscale) end
+
+	VideoOptionsFrameOkay:HookScript("OnClick", NeedReloadUI)
+	VideoOptionsFrameApply:HookScript("OnClick", NeedReloadUI)
+
+	self:UnregisterEvent("PLAYER_ENTERING_WORLD")
+end)
 
 local mult = 768 / string.match(GetCVar("gxResolution"), "%d+x(%d+)") / C["general"].uiscale
-local Scale = function(x)
-	return mult * math.floor(x / mult + 0.5)
-end
+local Scale = function(x) return mult * math.floor(x / mult + 0.5) end
 
 D.Scale = function(x) return Scale(x) end
 D.mult = mult
