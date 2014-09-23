f42dfd2034495022c62394bda9b6ea824631cd3e
liquidbase <liquid.base.x@googlemail.com>
2014-09-23 18:03:02 +0200

    Fixed SpellAnnoucement (no more reloading after login)

--- DuffedUI/core/func_Core.lua
+++ DuffedUI/core/func_Core.lua
@@ -300,30 +300,28 @@ RoleUpdater:SetScript("OnEvent", CheckRole)
 local waitTable = {}
 local waitFrame
 D.Delay = function(delay, func, ...)
-	if(type(delay) ~= "number" or type(func) ~= "function") then
-		return false
-	end
-	if(waitFrame == nil) then
-		waitFrame = CreateFrame("Frame","WaitFrame", UIParent)
-		waitFrame:SetScript("onUpdate",function (self, elapse)
+	if (type(delay) ~= "number") or (type(func) ~= "function") then return false end
+	if waitFrame == nil then
+		waitFrame = CreateFrame("Frame", "WaitFrame", UIParent)
+		waitFrame:SetScript("onUpdate", function(self, elapse)
 			local count = #waitTable
 			local i = 1
-			while(i <= count) do
+			while i <= count do
 				local waitRecord = tremove(waitTable, i)
 				local d = tremove(waitRecord, 1)
 				local f = tremove(waitRecord, 1)
 				local p = tremove(waitRecord, 1)
-				if(d>elapse) then
-				  tinsert(waitTable, i, {d - elapse, f, p})
-				  i = i + 1
+				if d > elapse then
+					tinsert(waitTable, i, {d - elapse, f, p})
+					i = i + 1
 				else
-				  count = count - 1
-				  f(unpack(p))
+					count = count - 1
+					f(unpack(p))
 				end
 			end
 		end)
 	end
-	tinsert(waitTable, {delay,func,{...}})
+	tinsert(waitTable, {delay, func, {...}})
 	return true
 end
 
@@ -339,9 +337,7 @@ D.CreateBtn = function(name, parent, w, h, tt_txt, txt)
 		GameTooltip:Show()
 	end)
 
-	b:SetScript("OnLeave", function(self)
-		GameTooltip:Hide()
-	end)
+	b:SetScript("OnLeave", function(self) GameTooltip:Hide() end)
 
 	b.text = b:CreateFontString(nil, "OVERLAY")
 	b.text:SetFontObject(font)
