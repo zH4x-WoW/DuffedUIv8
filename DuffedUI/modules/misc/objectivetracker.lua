582356bdc5666b50c30c1bbcfe69700dc7034b70
liquidbase <liquid.base.x@googlemail.com>
2014-11-14 11:48:06 +0100

    Added Questlevel display

--- DuffedUI/modules/misc/objectivetracker.lua
+++ DuffedUI/modules/misc/objectivetracker.lua
@@ -266,6 +266,83 @@ hooksecurefunc("AchievementObjectiveTracker_OnOpenDropDown", function(self)
 	UIDropDownMenu_AddButton(info, UIDROPDOWN_MENU_LEVEL)
 end)
 
+--[[Questlevel display]]
+local QuestLevelPatch = {}
+
+function GossipFrameUpdate_hook()
+	local buttonIndex = 1
+
+	local availableQuests = {GetGossipAvailableQuests()}
+	local numAvailableQuests = table.getn(availableQuests)
+	for i = 1, numAvailableQuests, 6 do
+		local titleButton = _G["GossipTitleButton" .. buttonIndex]
+		local title = "[" .. availableQuests[i + 1] .. "] " .. availableQuests[i]
+		local isTrivial = availableQuests[i + 2]
+		if isTrivial then titleButton:SetFormattedText(TRIVIAL_QUEST_DISPLAY, title) else titleButton:SetFormattedText(NORMAL_QUEST_DISPLAY, title) end
+		GossipResize(titleButton)
+		buttonIndex = buttonIndex + 1
+	end
+	if numAvailableQuests > 1 then buttonIndex = buttonIndex + 1 end
+
+	local activeQuests = {GetGossipActiveQuests()}
+	local numActiveQuests = table.getn(activeQuests)
+	for i = 1, numActiveQuests, 5 do
+		local titleButton = _G["GossipTitleButton" .. buttonIndex]
+		local title = "[" .. activeQuests[i + 1] .. "] " .. activeQuests[i]
+		local isTrivial = activeQuests[i + 2]
+		if isTrivial then titleButton:SetFormattedText(TRIVIAL_QUEST_DISPLAY, title) else titleButton:SetFormattedText(NORMAL_QUEST_DISPLAY, title) end
+		GossipResize(titleButton)
+		buttonIndex = buttonIndex + 1
+	end
+end
+
+function SetBlockHeader_hook()
+	for i = 1, GetNumQuestWatches() do
+		local questID, title, questLogIndex, numObjectives, requiredMoney, isComplete, startEvent, isAutoComplete, failureTime, timeElapsed, questType, isTask, isStory, isOnMap, hasLocalPOI = GetQuestWatchInfo(i)
+		if not questID then break end
+		local oldBlock = QUEST_TRACKER_MODULE:GetExistingBlock(questID)
+		if oldBlock then
+			local oldBlockHeight = oldBlock.height
+			local oldHeight = QUEST_TRACKER_MODULE:SetStringText(oldBlock.HeaderText, title, nil, OBJECTIVE_TRACKER_COLOR["Header"])
+			local newTitle = "[" .. select(2, GetQuestLogTitle(questLogIndex)) .. "] " .. title
+			local newHeight = QUEST_TRACKER_MODULE:SetStringText(oldBlock.HeaderText, newTitle, nil, OBJECTIVE_TRACKER_COLOR["Header"])
+			oldBlock:SetHeight(oldBlockHeight + newHeight - oldHeight)
+		end
+	end
+end
+
+function QuestLogQuests_hook(self, poiTable)
+	local numEntries, numQuests = GetNumQuestLogEntries()
+	local headerIndex = 0
+	for questLogIndex = 1, numEntries do
+		local title, level, suggestedGroup, isHeader, isCollapsed, isComplete, frequency, questID, startEvent, displayQuestID, isOnMap, hasLocalPOI, isTask, isStory = GetQuestLogTitle(questLogIndex)
+		if isOnMap and not isTask and not isHeader then
+			headerIndex = headerIndex + 1
+			local button = QuestLogQuests_GetTitleButton(headerIndex)
+			local oldBlockHeight = button:GetHeight()
+			local oldHeight = button.Text:GetStringHeight()
+			local newTitle = "[" .. level .. "] " .. button.Text:GetText()
+			button.Text:SetText(newTitle)
+			local newHeight = button.Text:GetStringHeight()
+			button:SetHeight(oldBlockHeight + newHeight - oldHeight)
+		end
+	end
+end
+
+function QuestInfo_hook(template, parentFrame, acceptButton, material, mapView)
+	local elementsTable = template.elements
+	for i = 1, #elementsTable, 3 do
+		if elementsTable[i] == QuestInfo_ShowTitle then
+			if QuestInfoFrame.questLog then
+				local questLogIndex = GetQuestLogSelection()
+				local level = select(2, GetQuestLogTitle(questLogIndex))
+				local newTitle = "[" .. level .. "] " .. QuestInfoTitleHeader:GetText()
+				QuestInfoTitleHeader:SetText(newTitle)
+			end
+		end
+	end
+end
+
 --[[execution]]--
 local ObjFhandler = CreateFrame("Frame")
 ObjFhandler:RegisterEvent("ADDON_LOADED")
@@ -285,4 +362,8 @@ if IsAddOnLoaded("Blizzard_ObjectiveTracker") then
 		if AddAutoQuestPopUp(questID, popUpType) then alterAQButton() end
 	end)     
 	hooksecurefunc(AUTO_QUEST_POPUP_TRACKER_MODULE, "Update", alterAQButton)
+	hooksecurefunc("GossipFrameUpdate", GossipFrameUpdate_hook)
+	hooksecurefunc(QUEST_TRACKER_MODULE, "Update", SetBlockHeader_hook)
+	hooksecurefunc("QuestLogQuests_Update", QuestLogQuests_hook)
+	hooksecurefunc("QuestInfo_Display", QuestInfo_hook)
 end
