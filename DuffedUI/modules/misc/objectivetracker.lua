d77f929ea22f01ed45f7379f68e0389d61d2ce03
liquidbase <liquid.base.x@googlemail.com>
2014-11-14 19:06:33 +0100

    Adjustments for Questlevel display

--- DuffedUI/modules/misc/objectivetracker.lua
+++ DuffedUI/modules/misc/objectivetracker.lua
@@ -295,6 +295,7 @@ function GossipFrameUpdate_hook()
 		buttonIndex = buttonIndex + 1
 	end
 end
+hooksecurefunc("GossipFrameUpdate", GossipFrameUpdate_hook)
 
 function SetBlockHeader_hook()
 	for i = 1, GetNumQuestWatches() do
@@ -302,14 +303,12 @@ function SetBlockHeader_hook()
 		if not questID then break end
 		local oldBlock = QUEST_TRACKER_MODULE:GetExistingBlock(questID)
 		if oldBlock then
-			local oldBlockHeight = oldBlock.height
-			local oldHeight = QUEST_TRACKER_MODULE:SetStringText(oldBlock.HeaderText, title, nil, OBJECTIVE_TRACKER_COLOR["Header"])
 			local newTitle = "[" .. select(2, GetQuestLogTitle(questLogIndex)) .. "] " .. title
-			local newHeight = QUEST_TRACKER_MODULE:SetStringText(oldBlock.HeaderText, newTitle, nil, OBJECTIVE_TRACKER_COLOR["Header"])
-			oldBlock:SetHeight(oldBlockHeight + newHeight - oldHeight)
+			QUEST_TRACKER_MODULE:SetStringText(oldBlock.HeaderText, newTitle, nil, OBJECTIVE_TRACKER_COLOR["Header"])
 		end
 	end
 end
+hooksecurefunc(QUEST_TRACKER_MODULE, "Update", SetBlockHeader_hook)
 
 function QuestLogQuests_hook(self, poiTable)
 	local numEntries, numQuests = GetNumQuestLogEntries()
@@ -328,20 +327,7 @@ function QuestLogQuests_hook(self, poiTable)
 		end
 	end
 end
-
-function QuestInfo_hook(template, parentFrame, acceptButton, material, mapView)
-	local elementsTable = template.elements
-	for i = 1, #elementsTable, 3 do
-		if elementsTable[i] == QuestInfo_ShowTitle then
-			if QuestInfoFrame.questLog then
-				local questLogIndex = GetQuestLogSelection()
-				local level = select(2, GetQuestLogTitle(questLogIndex))
-				local newTitle = "[" .. level .. "] " .. QuestInfoTitleHeader:GetText()
-				QuestInfoTitleHeader:SetText(newTitle)
-			end
-		end
-	end
-end
+hooksecurefunc("QuestLogQuests_Update", QuestLogQuests_hook)
 
 --[[Execution]]--
 local ObjFhandler = CreateFrame("Frame")
@@ -362,8 +348,4 @@ if IsAddOnLoaded("Blizzard_ObjectiveTracker") then
 		if AddAutoQuestPopUp(questID, popUpType) then alterAQButton() end
 	end)     
 	hooksecurefunc(AUTO_QUEST_POPUP_TRACKER_MODULE, "Update", alterAQButton)
-	hooksecurefunc("GossipFrameUpdate", GossipFrameUpdate_hook)
-	hooksecurefunc(QUEST_TRACKER_MODULE, "Update", SetBlockHeader_hook)
-	hooksecurefunc("QuestLogQuests_Update", QuestLogQuests_hook)
-	hooksecurefunc("QuestInfo_Display", QuestInfo_hook)
 end
