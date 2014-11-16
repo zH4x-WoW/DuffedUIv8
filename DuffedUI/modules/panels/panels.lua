7796f1137ecd5f6440a5c4f008c20dfd8fbbd8ef
liquidbase <liquid.base.x@googlemail.com>
2014-11-16 02:28:11 +0100

    Fixed parent and position of LayoutSwitcher

--- DuffedUI/modules/panels/panels.lua
+++ DuffedUI/modules/panels/panels.lua
@@ -156,8 +156,8 @@ chatmenu:SetScript("OnMouseDown", function(self, btn)
 	if btn == "LeftButton" then ToggleFrame(ChatMenu) end
 end)
 
-D.CreateBtn("MB_switch", DuffedUITabsRightBackground, 20, 20, L["buttons"]["ses_switch"], "S")
-if C["chat"].rbackground then MB_switch:Point("LEFT", DuffedUITabsRightBackground, "RIGHT", 2, 0) else MB_switch:Point("LEFT",DuffedUIChatMenu, "RIGHT", 2, 0) end
+D.CreateBtn("MB_switch", DuffedUIChatMenu, 20, 20, L["buttons"]["ses_switch"], "S")
+if C["chat"].rbackground then MB_switch:Point("LEFT", DuffedUITabsRightBackground, "RIGHT", 2, 0) else MB_switch:Point("RIGHT",DuffedUIChatMenu, "LEFT", -2, 0) end
 MB_switch:SetAttribute("macrotext1", "/switch")
 
 if DuffedUIMinimap then
