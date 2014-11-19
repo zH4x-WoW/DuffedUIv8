aaf41a9d8e2c09aa734fd39ad8a9305ff1436014
liquidbase <liquid.base.x@googlemail.com>
2014-11-19 19:15:57 +0100

    Fixed Transmogrification-Skin

--- DuffedUI/modules/skins/itemtransmogrify.lua
+++ DuffedUI/modules/skins/itemtransmogrify.lua
@@ -1,13 +1,9 @@
 local D, C, L = unpack(select(2, ...))
 
-----------------------------------------------------------------------------------------
---	Transmogrify skin (written by shestak)
-----------------------------------------------------------------------------------------
-
 local function LoadSkin()
-	TransmogrifyArtFrame:StripTextures()
-	TransmogrifyArtFrame:SetTemplate("Transparent")
-	TransmogrifyModelFrame:SetFrameLevel(TransmogrifyArtFrame:GetFrameLevel() + 2)
+	TransmogrifyFrame:StripTextures()
+	TransmogrifyFrame:SetTemplate("Transparent")
+	TransmogrifyModelFrame:SetFrameLevel(TransmogrifyFrame:GetFrameLevel() + 2)
 
 	local KillTextures = {
 		"TransmogrifyModelFrameLines",
@@ -19,9 +15,7 @@ local function LoadSkin()
 		"TransmogrifyFrameButtonFrameMoneyMiddle",
 	}
 
-	for _, texture in pairs(KillTextures) do
-		_G[texture]:Kill()
-	end
+	for _, texture in pairs(KillTextures) do _G[texture]:Kill() end
 
 	select(2, TransmogrifyModelFrame:GetRegions()):Kill()
 	TransmogrifyFrameButtonFrame:GetRegions():Kill()
@@ -29,14 +23,25 @@ local function LoadSkin()
 	TransmogrifyApplyButton:SkinButton(true)
 	TransmogrifyApplyButton:Point("BOTTOMRIGHT", TransmogrifyFrame, "BOTTOMRIGHT", -4, 4)
 	TransmogrifyArtFrameCloseButton:SkinCloseButton()
+	TransmogrifyArtFrame:StripTextures()
 
-	local slots = {"Head", "Shoulder", "Chest", "Waist", "Legs", "Feet",
-	"Wrist", "Hands", "Back", "MainHand", "SecondaryHand", "Ranged"}
-
+	local slots = {
+		"Head",
+		"Shoulder",
+		"Chest",
+		"Waist",
+		"Legs",
+		"Feet",
+		"Wrist",
+		"Hands",
+		"Back",
+		"MainHand",
+		"SecondaryHand"
+	}
 	for _, slot in pairs(slots) do
-		local icon = _G["TransmogrifyFrame"..slot.."SlotIconTexture"]
-		local slot = _G["TransmogrifyFrame"..slot.."Slot"]
-		
+		local icon = _G["TransmogrifyFrame" .. slot .. "SlotIconTexture"]
+		local slot = _G["TransmogrifyFrame" .. slot .. "Slot"]
+
 		if slot then
 			slot:StripTextures()
 			slot:StyleButton(false)
@@ -44,17 +49,48 @@ local function LoadSkin()
 			slot:CreateBackdrop("Default")
 			slot.backdrop:SetAllPoints()
 
-			icon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
+			icon:SetTexCoord(unpack(D.IconCoord))
 			icon:ClearAllPoints()
-			icon:Point("TOPLEFT", 2, -2)
-			icon:Point("BOTTOMRIGHT", -2, 2)
+			icon:SetInside()
 		end
 	end
-	
+
+	TransmogrifyConfirmationPopup:SetParent(UIParent)
 	TransmogrifyConfirmationPopup:StripTextures()
 	TransmogrifyConfirmationPopup:SetTemplate("Transparent")
 	TransmogrifyConfirmationPopup.Button1:SkinButton()
 	TransmogrifyConfirmationPopup.Button2:SkinButton()
+	TransmogrifyConfirmationPopupItemFrame1:SkinButton(true)
+	TransmogrifyConfirmationPopupItemFrame2:SkinButton(true)
+
+	EquipmentFlyoutFrameHighlight:Kill()
+	local function SkinItemFlyouts()
+		EquipmentFlyoutFrameButtons:StripTextures()
+
+		local i = 1
+		local button = _G["EquipmentFlyoutFrameButton" .. i]
+
+		while button do
+			local icon = _G["EquipmentFlyoutFrameButton" .. i .. "IconTexture"]
+			button:StyleButton(false)
+
+			icon:SetTexCoord(unpack(D.IconCoord))
+			button:GetNormalTexture():SetTexture(nil)
+
+			icon:SetInside()
+			button:SetFrameLevel(button:GetFrameLevel() + 2)
+			if not button.backdrop then
+				button:CreateBackdrop("Default")
+				button.backdrop:SetAllPoints()
+			end
+			i = i + 1
+			button = _G["EquipmentFlyoutFrameButton" .. i]
+		end
+	end
+
+	--[[Swap item flyout frame]]--
+	EquipmentFlyoutFrame:HookScript("OnShow", SkinItemFlyouts)
+	hooksecurefunc("EquipmentFlyout_Show", SkinItemFlyouts)
 end
 
 D.SkinFuncs["Blizzard_ItemAlterationUI"] = LoadSkin
