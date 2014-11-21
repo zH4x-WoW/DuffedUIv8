4552ae1434db94bde4cb527ae0cb9615dfff980c
liquidbase <liquid.base.x@googlemail.com>
2014-11-21 14:16:46 +0100

    Teststuff

--- DuffedUI/modules/misc/threatbar.lua
+++ DuffedUI/modules/misc/threatbar.lua
@@ -31,18 +31,21 @@ DuffedUIThreatBar.bg:SetAllPoints(DuffedUIThreatBar)
 DuffedUIThreatBar.bg:SetTexture(.1, .1, .1)
 
 local function OnEvent(self, event, ...)
-	local party = GetNumGroupMembers()
+	--[[local party = GetNumGroupMembers()
 	local raid = GetNumGroupMembers()
-	local pet = select(1, HasPetUI())
+	local pet = select(1, HasPetUI())]]
+	local party, raid, pet = IsInGroup(), IsInRaid(), UnitExists("pet")
 	if event == "PLAYER_ENTERING_WORLD" then
 		self:Hide()
 		self:UnregisterEvent("PLAYER_ENTERING_WORLD")
 	elseif event == "PLAYER_REGEN_ENABLED" then
 		self:Hide()
 	elseif event == "PLAYER_REGEN_DISABLED" then
-		if (party > 0) or (raid > 0) or (pet == 1) then self:Show() else self:Hide() end
+		--if party > 0 or raid > 0 or pet == 1 then self:Show() else self:Hide() end
+		if party or raid or pet then self:Show() else self:Hide() end
 	else
-		if (InCombatLockdown()) and (party > 0 or raid > 0 or pet == 1) then self:Show() else self:Hide() end
+		--if (InCombatLockdown()) and (party > 0 or raid > 0 or pet == 1) then self:Show() else self:Hide() end
+		if (InCombatLockdown()) and (party or raid or pet) then self:Show() else self:Hide() end
 	end
 end
 
