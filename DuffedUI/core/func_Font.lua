5ba63c37173fe9abe8fb109f4767eba3020aa5f2
liquidbase <liquid.base.x@googlemail.com>
2014-12-16 08:47:38 +0100

    Attempt to fix font issue on chinese clients

--- DuffedUI/core/func_Font.lua
+++ DuffedUI/core/func_Font.lua
@@ -1,5 +1,22 @@
 local D, C, L = unpack(select(2, ...))
 
+if D.Client == "ruRU" then
+	C["media"].font = C["media"].ru_font
+	C["media"].dmgfont = C["media"].ru_dmgfont
+elseif D.Client == "zhTW" then
+	C["media"].font = C["media"].tw_font
+	C["media"].dmgfont = C["media"].tw_dmgfont
+elseif D.Client == "koKR" then
+	C["media"].font = C["media"].kr_font
+	C["media"].dmgfont = C["media"].kr_dmgfont
+elseif D.Client == "frFR" then
+	C["media"].font = C["media"].fr_font
+	C["media"].dmgfont = C["media"].fr_dmgfont
+elseif D.Client == "zhCN" then
+	C["media"].font = C["media"].cn_font
+	C["media"].dmgfont = C["media"].cn_dmgfont
+end
+
 local duffed = CreateFont("DuffedUI")
 duffed:SetFont(C["media"].font, C["font"].duffeduisize, "THINOUTLINE")
 
@@ -17,24 +34,4 @@ local MakeFont = {
 
 D.Font = function(value)
 	if MakeFont[value] then return MakeFont[value] else return MakeFont["DuffedUI"] end
-end
-
-if D.Client == "ruRU" then
-	C["media"].font = C["media"].ru_font
-	C["media"].dmgfont = C["media"].ru_dmgfont
-elseif D.Client == "zhTW" then
-	C["media"].font = C["media"].tw_font
-	C["media"].dmgfont = C["media"].tw_dmgfont
-elseif D.Client == "koKR" then
-	C["media"].font = C["media"].kr_font
-	C["media"].dmgfont = C["media"].kr_dmgfont
-elseif D.Client == "frFR" then
-	C["media"].font = C["media"].fr_font
-	C["media"].dmgfont = C["media"].fr_dmgfont
-elseif D.Client == "zhCN" then
-	C["media"].font = C["media"].cn_font
-	C["media"].dmgfont = C["media"].cn_dmgfont
-elseif D.Client == "zhTW" then
-	C["media"].font = C["media"].tw_font
-	C["media"].dmgfont = C["media"].tw_dmgfont
 end
