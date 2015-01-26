a0ec174c0b67de0604deeb73a79812c23be0db95
liquidbase <liquid.base.x@googlemail.com>
2015-01-26 08:41:00 +0100

    Fixed Micromenu for 6.1.0 (Collections)

--- DuffedUI/modules/misc/micromenu.lua
+++ DuffedUI/modules/misc/micromenu.lua
@@ -6,7 +6,16 @@ D.MicroMenu = {
 	{text = SPELLBOOK_ABILITIES_BUTTON, func = function() ToggleFrame(SpellBookFrame) end},
 	{text = TALENTS_BUTTON, func = function() if not PlayerTalentFrame then TalentFrame_LoadUI() end ShowUIPanel(PlayerTalentFrame) end},
 	{text = ACHIEVEMENT_BUTTON, func = function() ToggleAchievementFrame() end},
-	{text = MOUNTS_AND_PETS, func = function() TogglePetJournal() end},
+	{text = MOUNTS_AND_PETS,
+	func = function()
+		--[[Remove me after Patch 6.1.0 hits live server]]--
+		if not D.Patch == "6.1.0" then
+			TogglePetJournal()
+		else
+		--[[Remove me after Patch 6.1.0 hits live server]]--
+			ToggleCollectionsJournal()
+		end
+	end},
 	{text = SOCIAL_BUTTON, func = function() ToggleFriendsFrame() end},
 	{text = COMPACT_UNIT_FRAME_PROFILE_AUTOACTIVATEPVE.." / "..COMPACT_UNIT_FRAME_PROFILE_AUTOACTIVATEPVP, func = function() PVEFrame_ToggleFrame() end},
 	{text = ACHIEVEMENTS_GUILD_TAB,
@@ -33,5 +42,5 @@ taint:RegisterEvent("ADDON_LOADED")
 taint:SetScript("OnEvent", function(self, event, addon)
 	if addon ~= "DuffedUI" then return end
 	ToggleFrame(SpellBookFrame)
-	PetJournal_LoadUI()
+	if D.Patch == "6.1.0" then ToggleCollectionsJournal() else PetJournal_LoadUI() end
 end)
