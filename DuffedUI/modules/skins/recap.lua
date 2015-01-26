a6aaf2c5da51cd73a2317e8032e1534b2df0400f
liquidbase <liquid.base.x@googlemail.com>
2015-01-26 10:07:52 +0100

    DeathRecap-skin

--- DuffedUI/modules/skins/recap.lua
+++ DuffedUI/modules/skins/recap.lua
@@ -1,1 +1,9 @@
 local D, C, L = unpack(select(2, ...))
+
+local function RecapSkin()
+	DeathRecapFrame:StripTextures()
+	DeathRecapFrame:SetTemplate("Transparent")
+	DeathRecapFrame.CloseXButton:SkinCloseButton()
+end
+
+D.SkinFuncs["Blizzard_DeathRecap"] = RecapSkin
