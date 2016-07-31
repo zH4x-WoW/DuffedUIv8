2ab2fcf59abd8f5fe4bfca36e86c6ba187348507
liquidbase <liquid.base.x@gmail.com>
2016-07-31 23:27:43 +0200

    Typo

--- DuffedUI/modules/misc/objectivetracker.lua
+++ DuffedUI/modules/misc/objectivetracker.lua
@@ -94,7 +94,7 @@ local function CreateQuestTag(level, questTag, frequency)
 	
 	local color = D.RGBToHex(unpack(C["media"].datatextcolor1))
 	local col = GetQuestDifficultyColor(level)
-	if frequency == 2 then tag = tag.."!" elseif frequency == 3 then tag = tag.."!!" end
+	if frequency == 2 then tag = tag.."*" elseif frequency == 3 then tag = tag.."**" end
 	if tag ~= "" then tag = (color.."%s|r"):format(tag) end
 	tag = ("[|cff%2x%2x%2x%s|r%s|cff%1$2x%2$2x%3$2x|r] "):format(col.r * 255, col.g * 255, col.b * 255, level, tag) 
 	return tag
