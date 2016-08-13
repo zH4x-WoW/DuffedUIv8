d560e87f14cc0666e97702caa48323149b10d36f
liquidbase <liquid.base.x@gmail.com>
2016-08-14 01:42:33 +0200

    More Strings for shorten loot

--- DuffedUI/modules/plugins/misc.lua
+++ DuffedUI/modules/plugins/misc.lua
@@ -396,12 +396,21 @@ if C["misc"].gold == true then
 		LOOT_ITEM_SELF = "+ %s"
 		LOOT_ITEM_SELF_MULTIPLE = "+ %s x %d"
 		LOOT_ITEM_BONUS_ROLL_SELF = "+ %s"
+		LOOT_ITEM_BONUS_ROLL_SELF_MULTIPLE = "+ %s x %d (Bonus)"
 		LOOT_ITEM_CREATED_SELF = "+ %s"
+		LOOT_ITEM_CREATED_SELF_MULTIPLE = "+ %s x %d"
 		LOOT_ITEM_REFUND = "+ %s"
+		LOOT_ITEM_REFUND_MULTIPLE = "+ %s x %d"
 		ERR_QUEST_REWARD_ITEM_S = "+ %s"
 		CURRENCY_GAINED = "+ %s"
 		CURRENCY_GAINED_MULTIPLE = "+ %s x %d"
-		CURRENCY_GAINED_MULTIPLE_BONUS = "+ %s x %d (Bonus Objective)";
+		CURRENCY_GAINED_MULTIPLE_BONUS = "+ %s x %d (Bonus Objective)"
+		LOOT_ITEM = "+ %s => %s"
+		LOOT_ITEM_BONUS_ROLL = "+ %s => %s (Bonus)"
+		LOOT_ITEM_BONUS_ROLL_MULTIPLE = "+ %s => %s x %d"
+		LOOT_ITEM_MULTIPLE = "+ %s => %sx%d"
+		LOOT_ITEM_PUSHED = "+ %s => %s"
+		LOOT_ITEM_PUSHED_MULTIPLE = "+ %s => %s x %d"
 	end
 	frame:SetScript("OnEvent", eventHandler)
 end
