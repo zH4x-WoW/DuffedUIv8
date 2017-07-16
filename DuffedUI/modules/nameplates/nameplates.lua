08bf5fe66966688f270cd2b45b361083cbb71b42
liquidbase <liquid.base.x@gmail.com>
2017-07-16 12:38:56 +0200

    Added Name to nameplates, adjust fontsize and bordersize

--- DuffedUI/modules/nameplates/nameplates.lua
+++ DuffedUI/modules/nameplates/nameplates.lua
@@ -9,7 +9,7 @@ ns._Headers = {}
 
 local class = select(2, UnitClass("player"))
 local texture = C["media"]["normTex"]
-local f, fs, ff = C["media"].font, 11, "THINOUTLINE"
+local f, fs, ff = C["media"]["font"], 7, "THINOUTLINE"
 local nWidth, nHeight = C["nameplate"]["platewidth"], C["nameplate"]["plateheight"]
 local backdrop = {
 	bgFile = C["media"].blank,
@@ -24,9 +24,8 @@ D["ConstructNameplates"] = function(self)
 	local health = CreateFrame("StatusBar", nil, self)
 	health:SetAllPoints()
 	health:SetStatusBarTexture(texture)
-	health.colorHealth = true
 	health.colorTapping = true
-	health.colorDisconnected = true
+	health.colorReaction = true
 	if C["nameplate"]["classcolor"] then
 		health.colorClass = true
 	else
@@ -35,9 +34,9 @@ D["ConstructNameplates"] = function(self)
 
 	-- health border
 	local HealthBorder = CreateFrame("Frame", nil, health)
-	HealthBorder:Point("TOPLEFT", health, "TOPLEFT", D.Scale(-2), D.Scale(2))
-	HealthBorder:Point("BOTTOMRIGHT", health, "BOTTOMRIGHT", D.Scale(2), D.Scale(-2))
-	HealthBorder:SetTemplate("Default")
+	HealthBorder:Point("TOPLEFT", health, "TOPLEFT", -1, 1)
+	HealthBorder:Point("BOTTOMRIGHT", health, "BOTTOMRIGHT", 1, -1)
+	HealthBorder:SetTemplate("Transparent")
 	HealthBorder:SetFrameLevel(2)
 	self.HealthBorder = HealthBorder
 
@@ -46,10 +45,19 @@ D["ConstructNameplates"] = function(self)
 	bg:SetAllPoints()
 	bg:SetColorTexture(.2, .2, .2)
 
+	-- name
+	name = health:CreateFontString(nil, "OVERLAY")
+	name:Point("LEFT", health, "LEFT", 0, 10)
+	name:SetJustifyH("LEFT")
+	name:SetFont(f, fs, ff)
+	name:SetShadowOffset(1.25, -1.25)
+	self:Tag(name, "[DuffedUI:getnamecolor][DuffedUI:namelong] [DuffedUI:diffcolor][level] [shortclassification]")
+
 	-- size
 	self:SetSize(nWidth, nHeight)
 	self:SetPoint("CENTER", 0, 0)
 
 	-- Init
 	self.Health = health
+	self.Name = name
 end
