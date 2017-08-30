cbd6583b562f6d45e5275db9a0ed4a794f694e16
liquidbase <liquid.base.x@gmail.com>
2017-08-30 14:35:05 +0200

    Skin-Update => ArtifactUI

--- DuffedUI/modules/skins/artifactui.lua
+++ DuffedUI/modules/skins/artifactui.lua
@@ -8,9 +8,7 @@ local function ArtifactSkin()
 	ArtifactFrame.CloseButton:SkinCloseButton()
 	ArtifactFrame.BorderFrame:StripTextures()
 
-	for i = 1, 2 do
-		_G["ArtifactFrameTab" .. i]:SkinTab()
-	end
+	for i = 1, 2 do _G["ArtifactFrameTab" .. i]:SkinTab() end
 	ArtifactFrameTab1:ClearAllPoints()
 	ArtifactFrameTab1:SetPoint("TOPLEFT", ArtifactFrame, "BOTTOMLEFT", 0, -2)
 	ArtifactFrame.ForgeBadgeFrame.CircleMask:Hide()
@@ -19,12 +17,14 @@ local function ArtifactSkin()
 	ArtifactFrame.ForgeBadgeFrame.ForgeLevelBackground:SetPoint("RIGHT", ArtifactFrame.PerksTab.TitleContainer.ArtifactName, "LEFT", 0, -12)
 
 	-- Artifact Relic Forge
-	ArtifactRelicForgeFrame:StripTextures()
-	ArtifactRelicForgeFrame:SetTemplate("Transparent")
-	ArtifactRelicForgeFrameCloseButton:SkinCloseButton()
-	ArtifactRelicForgeFrame.TitleContainer.RelicSlot1.AttuneButton:SkinButton()
-	ArtifactRelicForgeFrame.TitleContainer.RelicSlot2.AttuneButton:SkinButton()
-	ArtifactRelicForgeFrame.TitleContainer.RelicSlot3.AttuneButton:SkinButton()
+	if ArtifactRelicForgeFrame:IsShown() then
+		ArtifactRelicForgeFrame:StripTextures()
+		ArtifactRelicForgeFrame:SetTemplate("Transparent")
+		ArtifactRelicForgeFrameCloseButton:SkinCloseButton()
+		ArtifactRelicForgeFrame.TitleContainer.RelicSlot1.AttuneButton:SkinButton()
+		ArtifactRelicForgeFrame.TitleContainer.RelicSlot2.AttuneButton:SkinButton()
+		ArtifactRelicForgeFrame.TitleContainer.RelicSlot3.AttuneButton:SkinButton()
+	end
 
 	-- Link ArtifactSpells to Chat => Shift + Click
 	local oldOnClick = ArtifactPowerButtonMixin.OnClick
