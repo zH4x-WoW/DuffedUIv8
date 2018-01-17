7bc083ef28c8d8f88c751fa53934748985b0d4e5
liquidbase <liquid.base.x@gmail.com>
2018-01-17 08:53:15 +0100

    Nothing Special

--- DuffedUI/core/constants.lua
+++ DuffedUI/core/constants.lua
@@ -1,62 +1,62 @@
 local D, C, L = unpack(select(2, ...))
 
-D['Dummy'] = function() return end
-D['MyName'] = select(1, UnitName('player'))
-D['Class'] = select(2, UnitClass('player'))
-D['MyRace'] = select(2, UnitRace('player'))
-D['Level'] = UnitLevel('player')
-D['MyRealm'] = GetRealmName()
-D['Client'] = GetLocale()
-D['Resolution'] = ({GetScreenResolutions()})[GetCurrentResolution()] or GetCVar('gxWindowedResolution')
-D['ScreenHeight'] = tonumber(string.match(D['Resolution'], '%d+x(%d+)'))
-D['ScreenWidth'] = tonumber(string.match(D['Resolution'], '(%d+)x+%d'))
-D['Version'] = GetAddOnMetadata('DuffedUI', 'Version')
-D['VersionNumber'] = tonumber(D['Version'])
-D['Patch'], D['BuildText'], D['ReleaseDate'], D['Toc'] = GetBuildInfo()
-D['build'] = tonumber(D['BuildText'])
-D['InfoLeftRightWidth'] = 370
-D['IconCoord'] = {.08, .92, .08, .92}
-D['Guild'] = select(1, GetGuildInfo('player'))
+D["Dummy"] = function() return end
+D["MyName"] = select(1, UnitName("player"))
+D["Class"] = select(2, UnitClass("player"))
+D["MyRace"] = select(2, UnitRace("player"))
+D["Level"] = UnitLevel("player")
+D["MyRealm"] = GetRealmName()
+D["Client"] = GetLocale()
+D["Resolution"] = ({GetScreenResolutions()})[GetCurrentResolution()] or GetCVar("gxWindowedResolution")
+D["ScreenHeight"] = tonumber(string.match(D.Resolution, "%d+x(%d+)"))
+D["ScreenWidth"] = tonumber(string.match(D.Resolution, "(%d+)x+%d"))
+D["Version"] = GetAddOnMetadata("DuffedUI", "Version")
+D["VersionNumber"] = tonumber(D.Version)
+D["Patch"], D["BuildText"], D["ReleaseDate"], D["Toc"] = GetBuildInfo()
+D["build"] = tonumber(D["BuildText"])
+D["InfoLeftRightWidth"] = 370
+D["IconCoord"] = {.08, .92, .08, .92}
+D["Guild"] = select(1, GetGuildInfo("player"))
 
-D['Credits'] = {
-	'Dejablue',
-	'Tukz',
-	'Hydra',
-	'Elv',
-	'Azilroka',
-	'Caith',
-	'Ishtara',
-	'Hungtar',
-	'Tulla',
-	'P3lim',
-	'Alza',
-	'Roth',
-	'Tekkub',
-	'Shestak',
-	'Caellian',
-	'Haleth',
-	'Nightcracker',
-	'Haste',
-	'humfras aka Shinizu',
-	'Hizuro',
-	'Duugu',
-	'Phanx',
-	'ObbleYeah',
+D["Credits"] = {
+	"Dejablue",
+	"Tukz",
+	"Hydra",
+	"Elv",
+	"Azilroka",
+	"Caith",
+	"Ishtara",
+	"Hungtar",
+	"Tulla",
+	"P3lim",
+	"Alza",
+	"Roth",
+	"Tekkub",
+	"Shestak",
+	"Caellian",
+	"Haleth",
+	"Nightcracker",
+	"Haste",
+	"humfras aka Shinizu",
+	"Hizuro",
+	"Duugu",
+	"Phanx",
+	"ObbleYeah",
 }
 
-D['DuffedCredits'] = {
-	'Darryn/Nyle',
-	'Hizuro',
-	'Chrisey',
-	'Niraya',
-	'Rolfman',
-	'siweia',
-	'Kahad',
-	'Blinks',
-	'Digawen',
+D["DuffedCredits"] = {
+	"Darryn/Nyle",
+	"Hizuro",
+	"Chrisey",
+	"Niraya",
+	"Rolfman",
+	"siweia",
+	"Kahad",
+	"Blinks",
+	"Digawen",
 }
 
-D['CoAutor'] = {
-	'Djriff',
-	'Rav',
+D["CoAutor"] = {
+	"Djriff",
+	"Rav",
 }
--- DuffedUI/core/video.lua
+++ DuffedUI/core/video.lua
@@ -1,10 +1,10 @@
 local D, C, L = unpack(select(2, ...))
 
 local RequireRestart = false
-if C['general']['autoscale'] then C['general']['uiscale'] = min(2, max(0.32, 768 / string.match(D['Resolution'], '%d+x(%d+)'))) end
+if C["general"].autoscale then C["general"].uiscale = min(2, max(0.32, 768 / string.match(D.Resolution, "%d+x(%d+)"))) end
 
-D.CreatePopup['CLIENT_RESTART'] = {
-	Question = L['misc']['Resolution'],
+D.CreatePopup["CLIENT_RESTART"] = {
+	Question = L["misc"]["Resolution"],
 	Answer1 = ACCEPT,
 	Answer2 = CANCEL,
 	Function1 = function(self)
@@ -16,22 +16,22 @@ D.CreatePopup['CLIENT_RESTART'] = {
 	end,
 }
 
-local Graphic = CreateFrame('Frame')
-Graphic:RegisterEvent('PLAYER_ENTERING_WORLD')
-Graphic:SetScript('OnEvent', function(self, event)
-	if (event == 'DISPLAY_SIZE_CHANGED') then
-		if C['general']['autoscale'] and not RequireRestart then D['ShowPopup']('CLIENT_RESTART') end
+local Graphic = CreateFrame("Frame")
+Graphic:RegisterEvent("PLAYER_ENTERING_WORLD")
+Graphic:SetScript("OnEvent", function(self, event)
+	if (event == "DISPLAY_SIZE_CHANGED") then
+		if C["general"].autoscale and not RequireRestart then D.ShowPopup("CLIENT_RESTART") end
 		RequireRestart = true
 	else
-		local UseUIScale = GetCVar('useUiScale')
+		local UseUIScale = GetCVar("useUiScale")
 
-		if (UseUIScale ~= '1') then SetCVar('useUiScale', 1) end
-		if (format('%.2f', GetCVar('uiScale')) ~= format('%.2f', C['general']['uiscale'])) then SetCVar('uiScale', C['general']['uiscale']) end
+		if (UseUIScale ~= "1") then SetCVar("useUiScale", 1) end
+		if (format("%.2f", GetCVar("uiScale")) ~= format("%.2f", C["general"].uiscale)) then SetCVar("uiScale", C["general"].uiscale) end
 
 		--[[Allow 4K and WQHD Resolution with UIScale lower than 0.64]]--
-		if (C['general']['uiscale'] < 0.64) then UIParent:SetScale(C['general']['uiscale']) end
+		if (C["general"].uiscale < 0.64) then UIParent:SetScale(C["general"].uiscale) end
 
-		self:UnregisterEvent('PLAYER_ENTERING_WORLD')
-		self:RegisterEvent('DISPLAY_SIZE_CHANGED')
+		self:UnregisterEvent("PLAYER_ENTERING_WORLD")
+		self:RegisterEvent("DISPLAY_SIZE_CHANGED")
 	end
 end)
