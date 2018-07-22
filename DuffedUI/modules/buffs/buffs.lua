6634762641e0e9ba17d6a041eb279649ebbb2273
liquidbase <liquid.base.x@gmail.com>
2018-07-22 15:09:11 +0200

    Fixing scaling issue on buffs and debuffs

--- DuffedUI/modules/buffs/buffs.lua
+++ DuffedUI/modules/buffs/buffs.lua
@@ -1,28 +1,28 @@
 local D, C, L = unpack(select(2, ...))
 
 --[[ This is a forked file by Haste ]]--
-local frame = CreateFrame("Frame", "DuffedUIAuras")
+local frame = CreateFrame('Frame', 'DuffedUIAuras')
 frame.content = {}
 
 local icon
-local faction = UnitFactionGroup("player")
-local flash = C["auras"].flash
-local sexID = UnitSex("player")
-local sex = "male"
+local faction = UnitFactionGroup('player')
+local flash = C['auras'].flash
+local sexID = UnitSex('player')
+local sex = 'male'
 local race = D.MyRace
-local f, fs, ff = C["media"].font, 11, "THINOUTLINE"
+local f, fs, ff = C['media'].font, 11, 'THINOUTLINE'
 
-if sexID == 3 or race == "Pandaren" then sex = "female" end
-if race == "Scourge" then race = "Undead" end
+if sexID == 3 or race == 'Pandaren' then sex = 'female' end
+if race == 'Scourge' then race = 'Undead' end
 
-local proxyicon = "Interface\\Icons\\Achievement_character_"..string.lower(race).."_"..sex
+local proxyicon = 'Interface\\Icons\\Achievement_character_'..string.lower(race)..'_'..sex
 
-if race == "Goblin" then
-	if sex == "male" then proxyicon = "Interface\\Icons\\Achievement_goblinhead" else proxyicon = "Interface\\Icons\\Achievement_femalegoblinhead" end
+if race == 'Goblin' then
+	if sex == 'male' then proxyicon = 'Interface\\Icons\\Achievement_goblinhead' else proxyicon = 'Interface\\Icons\\Achievement_femalegoblinhead' end
 end
 
-if race == "Worgen" then
-	proxyicon = "Interface\\Icons\\Achievement_worganhead"
+if race == 'Worgen' then
+	proxyicon = 'Interface\\Icons\\Achievement_worganhead'
 end
 
 local StartStopFlash = function(self, timeLeft)
@@ -48,8 +48,8 @@ local OnUpdate = function(self, elapsed)
 	if timeLeft <= 0 then
 		self.timeLeft = nil
 
-		self.Duration:SetText("")
-		return self:SetScript("OnUpdate", nil)
+		self.Duration:SetText('')
+		return self:SetScript('OnUpdate', nil)
 	else
 		local text = D.FormatTime(timeLeft)
 		local r, g, b = oUFDuffedUI.ColorGradient(self.timeLeft, self.Dur, .8, 0, 0, .8, .8, 0, 0, .8, 0)
@@ -71,14 +71,14 @@ local OnUpdate = function(self, elapsed)
 end
 
 local UpdateAura = function(self, index)
-	local name, texture, count, dtype, duration, expirationTime, caster, isStealable, shouldConsolidate, spellID, canApplyAura, isBossDebuff = UnitAura(self:GetParent():GetAttribute"unit", index, self.filter)
+	local name, texture, count, dtype, duration, expirationTime, caster, isStealable, shouldConsolidate, spellID, canApplyAura, isBossDebuff = UnitAura(self:GetParent():GetAttribute'unit', index, self.filter)
 	local consolidate = self.consolidate
 	if name then
 		if (duration > 0 and expirationTime and not consolidate) then
 			local timeLeft = expirationTime - GetTime()
 			if not self.timeLeft then
 				self.timeLeft = timeLeft
-				self:SetScript("OnUpdate", OnUpdate)
+				self:SetScript('OnUpdate', OnUpdate)
 			else
 				self.timeLeft = timeLeft
 			end
@@ -87,23 +87,23 @@ local UpdateAura = function(self, index)
 			if flash then StartStopFlash(self.Animation, timeLeft) end
 
 			self.Bar:SetMinMaxValues(0, duration)
-			if not C["auras"].classictimer then self.Holder:Show() end
+			if not C['auras'].classictimer then self.Holder:Show() end
 		else
 			if flash then self.Animation:Stop() end
 			self.timeLeft = nil
-			self.Duration:SetText("")
-			self:SetScript("OnUpdate", nil)
+			self.Duration:SetText('')
+			self:SetScript('OnUpdate', nil)
 
 			local min, max  = self.Bar:GetMinMaxValues()
 			self.Bar:SetValue(max)
 			self.Bar:SetStatusBarColor(0, .8, 0)
-			if not C["auras"].classictimer then self.Holder:Hide() end
+			if not C['auras'].classictimer then self.Holder:Hide() end
 		end
 
-		if count > 1 then self.Count:SetText(count) else self.Count:SetText("") end
+		if count > 1 then self.Count:SetText(count) else self.Count:SetText('') end
 
-		if self.filter == "HARMFUL" then
-			local color = DebuffTypeColor[dtype or "none"]
+		if self.filter == 'HARMFUL' then
+			local color = DebuffTypeColor[dtype or 'none']
 			self:SetBackdropBorderColor(color.r * 3 / 5, color.g * 3 / 5, color.b * 3 / 5)
 			self.Holder:SetBackdropBorderColor(color.r * 3 / 5, color.g * 3 / 5, color.b * 3 / 5)
 		end
@@ -113,111 +113,111 @@ local UpdateAura = function(self, index)
 end
 
 local UpdateTempEnchant = function(self, slot)
-	self.Icon:SetTexture(GetInventoryItemTexture("player", slot))
+	self.Icon:SetTexture(GetInventoryItemTexture('player', slot))
 
 	local offset
 	local weapon = self:GetName():sub(-1)
 
-	if weapon:match("1") then offset = 2 elseif weapon:match("2") then offset = 5 end
+	if weapon:match('1') then offset = 2 elseif weapon:match('2') then offset = 5 end
 
 	local expiration = select(offset, GetWeaponEnchantInfo())
 
 	if expiration then
 		self.Dur = 3600
 		self.offset = offset
-		self:SetScript("OnUpdate", OnUpdate)
+		self:SetScript('OnUpdate', OnUpdate)
 	else
 		self.offset = nil
 		self.timeLeft = nil
-		self:SetScript("OnUpdate", nil)
+		self:SetScript('OnUpdate', nil)
 	end
 end
 
 local OnAttributeChanged = function(self, attribute, value)
-	if C["auras"].classictimer then self.Holder:Hide() else self.Duration:Hide() end
+	if C['auras'].classictimer then self.Holder:Hide() else self.Duration:Hide() end
 
-	if attribute == "index" then
+	if attribute == 'index' then
 		return UpdateAura(self, value)
-	elseif attribute == "target-slot" then
+	elseif attribute == 'target-slot' then
 		self.Bar:SetMinMaxValues(0, 3600)
 		return UpdateTempEnchant(self, value)
 	end
 end
 
 local Skin = function(self)
-	local proxy = self:GetName():sub(-11) == "ProxyButton"
-	local Icon = self:CreateTexture(nil, "BORDER")
+	local proxy = self:GetName():sub(-11) == 'ProxyButton'
+	local Icon = self:CreateTexture(nil, 'BORDER')
 	Icon:SetTexCoord(.07, .93, .07, .93)
-	Icon:Point("TOPLEFT", 2, -2)
-	Icon:Point("BOTTOMRIGHT", -2, 2)
+	Icon:Point('TOPLEFT', 2, -2)
+	Icon:Point('BOTTOMRIGHT', -2, 2)
 	self.Icon = Icon
 
-	local Count = self:CreateFontString(nil, "OVERLAY")
+	local Count = self:CreateFontString(nil, 'OVERLAY')
 	Count:SetFont(f, fs, ff)
-	Count:SetPoint("TOP", self, 1, -4)
+	Count:SetPoint('TOP', self, 1, -4)
 	self.Count = Count
 
 	if not proxy then
-		local Holder = CreateFrame("Frame", nil, self)
+		local Holder = CreateFrame('Frame', nil, self)
 		Holder:Size(self:GetWidth(), 7)
-		Holder:SetPoint("TOP", self, "BOTTOM", 0, -1)
-		Holder:SetTemplate("Transparent")
+		Holder:SetPoint('TOP', self, 'BOTTOM', 0, -1)
+		Holder:SetTemplate('Transparent')
 		self.Holder = Holder
 
-		local Bar = CreateFrame("StatusBar", nil, Holder)
+		local Bar = CreateFrame('StatusBar', nil, Holder)
 		Bar:SetInside()
-		Bar:SetStatusBarTexture(C["media"].blank)
+		Bar:SetStatusBarTexture(C['media'].blank)
 		Bar:SetStatusBarColor(0, .8, 0)
 		self.Bar = Bar
 
-		local Duration = self:CreateFontString(nil, "OVERLAY")
+		local Duration = self:CreateFontString(nil, 'OVERLAY')
 		Duration:SetFont(f, fs, ff)
-		Duration:SetPoint("BOTTOM", 1, -15)
+		Duration:SetPoint('BOTTOM', 1, -15)
 		self.Duration = Duration
 
 		if flash then
 			local Animation = self:CreateAnimationGroup()
-			Animation:SetLooping"BOUNCE"
+			Animation:SetLooping'BOUNCE'
 
-			local FadeOut = Animation:CreateAnimation"Alpha"
+			local FadeOut = Animation:CreateAnimation'Alpha'
 			FadeOut:SetDuration(.4)
-			FadeOut:SetSmoothing("IN_OUT")
+			FadeOut:SetSmoothing('IN_OUT')
 
 			self.Animation = Animation
 		end
 
-		self:SetScript("OnAttributeChanged", OnAttributeChanged)
+		self:SetScript('OnAttributeChanged', OnAttributeChanged)
 
-		self.filter = self:GetParent():GetAttribute"filter"
+		self.filter = self:GetParent():GetAttribute'filter'
 	else
-		local Overlay = self:CreateTexture(nil, "OVERLAY")
+		local Overlay = self:CreateTexture(nil, 'OVERLAY')
 		local x = self:GetWidth()
 		local y = self:GetHeight()
 		Overlay:SetTexture(proxyicon)
-		Overlay:SetPoint("CENTER")
+		Overlay:SetPoint('CENTER')
 		Overlay:Size(x - 2, y - 2)
 		Overlay:SetTexCoord(.07, .93, .07, .93)
 		self.Overlay = Overlay
 	end
 
-	self:SetTemplate("Transparent")
+	self:SetTemplate('Transparent')
 end
 
-frame:SetScript("OnEvent", function(self, event, ...) self[event](self, event, ...) end)
+frame:SetScript('OnEvent', function(self, event, ...) self[event](self, event, ...) end)
 
 function frame:PLAYER_ENTERING_WORLD()
 	for _, header in next, frame.content do
-		local child = header:GetAttribute"child1"
+		local child = header:GetAttribute'child1'
 		local i = 1
 		while(child) do
 			UpdateAura(child, child:GetID())
 
 			i = i + 1
-			child = header:GetAttribute("child" .. i)
+			child = header:GetAttribute('child' .. i)
 		end
 	end
 end
-frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+frame:RegisterEvent('PLAYER_ENTERING_WORLD')
 
 function frame:VARIABLES_LOADED()
 	for _, header in next, frame.content do
@@ -225,16 +225,16 @@ function frame:VARIABLES_LOADED()
 			local buffs = DuffedUIAurasPlayerBuffs
 			local debuffs = DuffedUIAurasPlayerDebuffs
 			local position = buffs:GetPoint()
-			if position:match("LEFT") then
-				buffs:SetAttribute("xOffset", 35)
-				buffs:SetAttribute("point", position)
-				debuffs:SetAttribute("xOffset", 35)
-				debuffs:SetAttribute("point", position)
+			if position:match('LEFT') then
+				buffs:SetAttribute('xOffset', 35)
+				buffs:SetAttribute('point', position)
+				debuffs:SetAttribute('xOffset', 35)
+				debuffs:SetAttribute('point', position)
 			end
 		end
 	end
 end
-frame:RegisterEvent("VARIABLES_LOADED")
+frame:RegisterEvent('VARIABLES_LOADED')
 
 for frame, func in next, {
 	Skin = Skin,
--- DuffedUI/modules/buffs/header.lua
+++ DuffedUI/modules/buffs/header.lua
@@ -1,38 +1,37 @@
 local D, C, L = unpack(select(2, ...))
-
-if not C["auras"].player then return end
+if not C['auras'].player then return end
 
 local frame = DuffedUIAuras
 local content = DuffedUIAuras.content
-local move = D["move"]
+local move = D['move']
 
 for _, frame in next, {
-	"DuffedUIAurasPlayerBuffs",
-	"DuffedUIAurasPlayerDebuffs",
+	'DuffedUIAurasPlayerBuffs',
+	'DuffedUIAurasPlayerDebuffs',
 } do
 	local header
 	local wrap
-	wrap = C["auras"].wrap
+	wrap = C['auras'].wrap
 
-	header = CreateFrame("Frame", frame, oUF_PetBattleFrameHider, "SecureAuraHeaderTemplate")
+	header = CreateFrame('Frame', frame, oUF_PetBattleFrameHider, 'SecureAuraHeaderTemplate')
 	header:SetClampedToScreen(true)
 	header:SetMovable(true)
-	header:SetAttribute("minHeight", 30)
-	header:SetAttribute("wrapAfter", wrap)
-	header:SetAttribute("wrapYOffset", -50)
-	header:SetAttribute("xOffset", -35)
+	header:SetAttribute('minHeight', 30)
+	header:SetAttribute('wrapAfter', wrap)
+	header:SetAttribute('wrapYOffset', -50)
+	header:SetAttribute('xOffset', -35)
 	header:CreateBackdrop()
 	header.backdrop:SetBackdropBorderColor(1, 0, 0)
-	header.backdrop:FontString("text", C["media"].font, 11)
-	header.backdrop.text:SetPoint("CENTER")
-	header.backdrop.text:SetText(L["move"]["buffs"])
+	header.backdrop:FontString('text', C['media'].font, 11)
+	header.backdrop.text:SetPoint('CENTER')
+	header.backdrop.text:SetText(L['move']['buffs'])
 	header.backdrop:SetAlpha(0)
-	header:SetAttribute("minWidth", wrap * 35)
-	header:SetAttribute("template", "DuffedUIAurasAuraTemplate")
-	header:SetAttribute("weaponTemplate", "DuffedUIAurasAuraTemplate")
+	header:SetAttribute('minWidth', wrap * 35)
+	header:SetAttribute('template', 'DuffedUIAurasAuraTemplate')
+	header:SetAttribute('weaponTemplate', 'DuffedUIAurasAuraTemplate')
 	header:SetSize(30, 30)
 
-	RegisterAttributeDriver(header, "unit", "[vehicleui] vehicle; player")
+	RegisterAttributeDriver(header, 'unit', '[vehicleui] vehicle; player')
 	table.insert(content, header)
 end
 
@@ -41,13 +40,15 @@ local debuffs = DuffedUIAurasPlayerDebuffs
 
 local filter = 0
 
-buffs:SetPoint("TOPRIGHT", Minimap, "TOPLEFT", -7, 2)
-buffs:SetAttribute("filter", "HELPFUL")
-buffs:SetAttribute("includeWeapons", 1)
+buffs:SetPoint('TOPRIGHT', Minimap, 'TOPLEFT', -7, 2)
+buffs:SetAttribute('filter', 'HELPFUL')
+buffs:SetAttribute('includeWeapons', 1)
+buffs:SetScale(C['general']['uiscale'])
 buffs:Show()
 move:RegisterFrame(buffs)
 
-debuffs:SetPoint("BOTTOMRIGHT", Minimap, "BOTTOMLEFT", -7, -2)
-debuffs:SetAttribute("filter", "HARMFUL")
+debuffs:SetPoint('BOTTOMRIGHT', Minimap, 'BOTTOMLEFT', -7, -2)
+debuffs:SetAttribute('filter', 'HARMFUL')
+debuffs:SetScale(C['general']['uiscale'])
 debuffs:Show()
 move:RegisterFrame(debuffs)
