1f1c0012ad1d41b39f8276d14583db4651cdfe18
liquidbase <liquid.base.x@gmail.com>
2018-07-22 15:03:48 +0200

    Fixing minimap and coord display

--- DuffedUI/modules/maps/minimap.lua
+++ DuffedUI/modules/maps/minimap.lua
@@ -1,44 +1,44 @@
 local D, C, L = unpack(select(2, ...))
 
-local move = D["move"]
-local DuffedUIMinimap = CreateFrame("Frame", "DuffedUIMinimap", oUF_PetBattleFrameHider)
+local move = D['move']
+local DuffedUIMinimap = CreateFrame('Frame', 'DuffedUIMinimap', oUF_PetBattleFrameHider)
 DuffedUIMinimap:SetTemplate()
-DuffedUIMinimap:RegisterEvent("ADDON_LOADED")
-DuffedUIMinimap:Point("TOPRIGHT", UIParent, "TOPRIGHT", -5, -5)
+DuffedUIMinimap:RegisterEvent('ADDON_LOADED')
+DuffedUIMinimap:Point('TOPRIGHT', UIParent, 'TOPRIGHT', -5, -5)
 DuffedUIMinimap:Size(144)
+DuffedUIMinimap:SetScale(C['general']['uiscale'])
 move:RegisterFrame(DuffedUIMinimap)
 
 MinimapCluster:Kill()
 Minimap:SetParent(DuffedUIMinimap)
 Minimap:ClearAllPoints()
-Minimap:Point("TOPLEFT", 2, -2)
-Minimap:Point("BOTTOMRIGHT", -2, 2)
+Minimap:Point('TOPLEFT', 2, -2)
+Minimap:Point('BOTTOMRIGHT', -2, 2)
 GarrisonLandingPageMinimapButton:Kill()
 MinimapBorder:Hide()
 MinimapBorderTop:Hide()
 MinimapZoomIn:Hide()
 MinimapZoomOut:Hide()
---MiniMapVoiceChatFrame:Hide()
 MinimapNorthTag:SetTexture(nil)
 MinimapZoneTextButton:Hide()
 MiniMapTracking:Hide()
 GameTimeFrame:Hide()
 
 MiniMapMailFrame:ClearAllPoints()
-MiniMapMailFrame:Point("TOPRIGHT", Minimap, -2, 0)
+MiniMapMailFrame:Point('TOPRIGHT', Minimap, -2, 0)
 MiniMapMailFrame:SetFrameLevel(Minimap:GetFrameLevel() + 1)
 MiniMapMailFrame:SetFrameStrata(Minimap:GetFrameStrata())
 MiniMapMailBorder:Hide()
-MiniMapMailIcon:SetTexture("Interface\\AddOns\\DuffedUI\\medias\\textures\\mail")
+MiniMapMailIcon:SetTexture('Interface\\AddOns\\DuffedUI\\medias\\textures\\mail')
 
-local DuffedUITicket = CreateFrame("Frame", "DuffedUITicket", DuffedUIMinimap)
+local DuffedUITicket = CreateFrame('Frame', 'DuffedUITicket', DuffedUIMinimap)
 DuffedUITicket:SetTemplate()
 DuffedUITicket:Size(DuffedUIMinimap:GetWidth() - 4, 24)
 DuffedUITicket:SetFrameLevel(Minimap:GetFrameLevel() + 4)
 DuffedUITicket:SetFrameStrata(Minimap:GetFrameStrata())
-DuffedUITicket:Point("TOP", 0, -2)
-DuffedUITicket:FontString("Text", C["media"].font, 11)
-DuffedUITicket.Text:SetPoint("CENTER")
+DuffedUITicket:Point('TOP', 0, -2)
+DuffedUITicket:FontString('Text', C['media'].font, 11)
+DuffedUITicket.Text:SetPoint('CENTER')
 DuffedUITicket.Text:SetText(HELP_TICKET_EDIT)
 DuffedUITicket:SetBackdropBorderColor(255/255, 243/255,  82/255)
 DuffedUITicket.Text:SetTextColor(255/255, 243/255,  82/255)
@@ -51,60 +51,60 @@ HelpOpenTicketButton:ClearAllPoints()
 HelpOpenTicketButton:SetAllPoints()
 HelpOpenTicketButton:SetHighlightTexture(nil)
 HelpOpenTicketButton:SetAlpha(0)
-HelpOpenTicketButton:HookScript("OnShow", function(self) DuffedUITicket:SetAlpha(1) end)
-HelpOpenTicketButton:HookScript("OnHide", function(self) DuffedUITicket:SetAlpha(0) end)
+HelpOpenTicketButton:HookScript('OnShow', function(self) DuffedUITicket:SetAlpha(1) end)
+HelpOpenTicketButton:HookScript('OnHide', function(self) DuffedUITicket:SetAlpha(0) end)
 
 MiniMapWorldMapButton:Hide()
 MiniMapInstanceDifficulty:ClearAllPoints()
 MiniMapInstanceDifficulty:SetParent(Minimap)
-MiniMapInstanceDifficulty:SetPoint("TOPLEFT", Minimap, "TOPLEFT", 0, 0)
+MiniMapInstanceDifficulty:SetPoint('TOPLEFT', Minimap, 'TOPLEFT', 0, 0)
 GuildInstanceDifficulty:ClearAllPoints()
 GuildInstanceDifficulty:SetParent(Minimap)
-GuildInstanceDifficulty:SetPoint("TOPLEFT", Minimap, "TOPLEFT", 0, 0)
+GuildInstanceDifficulty:SetPoint('TOPLEFT', Minimap, 'TOPLEFT', 0, 0)
 QueueStatusMinimapButton:SetParent(Minimap)
 QueueStatusMinimapButton:ClearAllPoints()
-QueueStatusMinimapButton:SetPoint("BOTTOMRIGHT", 0, 0)
+QueueStatusMinimapButton:SetPoint('BOTTOMRIGHT', 0, 0)
 QueueStatusMinimapButtonBorder:Kill()
 QueueStatusFrame:StripTextures()
-QueueStatusFrame:SetTemplate("Transparent")
-QueueStatusFrame:SetFrameStrata("HIGH")
+QueueStatusFrame:SetTemplate('Transparent')
+QueueStatusFrame:SetFrameStrata('HIGH')
 
 local function UpdateLFGTooltip()
 	local position = DuffedUIMinimap:GetPoint()
 	QueueStatusFrame:ClearAllPoints()
-	if position:match("BOTTOMRIGHT") then
-		QueueStatusFrame:SetPoint("BOTTOMRIGHT", QueueStatusMinimapButton, "BOTTOMLEFT", 0, 0)
-	elseif position:match("BOTTOM") then
-		QueueStatusFrame:SetPoint("BOTTOMLEFT", QueueStatusMinimapButton, "BOTTOMRIGHT", 4, 0)
-	elseif position:match("LEFT") then
-		QueueStatusFrame:SetPoint("TOPLEFT", QueueStatusMinimapButton, "TOPRIGHT", 4, 0)
+	if position:match('BOTTOMRIGHT') then
+		QueueStatusFrame:SetPoint('BOTTOMRIGHT', QueueStatusMinimapButton, 'BOTTOMLEFT', 0, 0)
+	elseif position:match('BOTTOM') then
+		QueueStatusFrame:SetPoint('BOTTOMLEFT', QueueStatusMinimapButton, 'BOTTOMRIGHT', 4, 0)
+	elseif position:match('LEFT') then
+		QueueStatusFrame:SetPoint('TOPLEFT', QueueStatusMinimapButton, 'TOPRIGHT', 4, 0)
 	else
-		QueueStatusFrame:SetPoint("TOPRIGHT", QueueStatusMinimapButton, "TOPLEFT", 0, 0)
+		QueueStatusFrame:SetPoint('TOPRIGHT', QueueStatusMinimapButton, 'TOPLEFT', 0, 0)
 	end
 end
-QueueStatusFrame:HookScript("OnShow", UpdateLFGTooltip)
+QueueStatusFrame:HookScript('OnShow', UpdateLFGTooltip)
 
 Minimap:EnableMouseWheel(true)
-Minimap:SetScript("OnMouseWheel", function(self, d)
+Minimap:SetScript('OnMouseWheel', function(self, d)
 	if d > 0 then _G.MinimapZoomIn:Click() elseif d < 0 then _G.MinimapZoomOut:Click() end
 end)
 
-Minimap:SetMaskTexture(C["media"].blank)
-function GetMinimapShape() return "SQUARE" end
-DuffedUIMinimap:SetScript("OnEvent", function(self, event, addon)
-	if addon == "Blizzard_TimeManager" then TimeManagerClockButton:Kill() end
+Minimap:SetMaskTexture(C['media'].blank)
+function GetMinimapShape() return 'SQUARE' end
+DuffedUIMinimap:SetScript('OnEvent', function(self, event, addon)
+	if addon == 'Blizzard_TimeManager' then TimeManagerClockButton:Kill() end
 end)
 
-Minimap:SetScript("OnMouseUp", function(self, btn)
+Minimap:SetScript('OnMouseUp', function(self, btn)
 	local xoff = 0
 	local position = DuffedUIMinimap:GetPoint()
 
-	if btn == "MiddleButton" or (IsShiftKeyDown() and btn == "RightButton") then
+	if btn == 'MiddleButton' or (IsShiftKeyDown() and btn == 'RightButton') then
 		if not DuffedUIMicroButtonsDropDown then return end
-		if position:match("RIGHT") then xoff = D.Scale(-160) end
-		Lib_EasyMenu(D.MicroMenu, DuffedUIMicroButtonsDropDown, "cursor", xoff, 0, "MENU", 2)
-	elseif btn == "RightButton" then
-		if position:match("RIGHT") then xoff = D.Scale(-8) end
+		if position:match('RIGHT') then xoff = D.Scale(-160) end
+		Lib_EasyMenu(D.MicroMenu, DuffedUIMicroButtonsDropDown, 'cursor', xoff, 0, 'MENU', 2)
+	elseif btn == 'RightButton' then
+		if position:match('RIGHT') then xoff = D.Scale(-8) end
 		ToggleDropDownMenu(nil, nil, MiniMapTrackingDropDown, DuffedUIMinimap, xoff, D.Scale(-2))
 	else
 		Minimap_OnClick(self)
@@ -112,34 +112,40 @@ Minimap:SetScript("OnMouseUp", function(self, btn)
 end)
 
 Minimap:EnableMouseWheel(true)
-Minimap:SetScript("OnMouseWheel", function(self, delta)
+Minimap:SetScript('OnMouseWheel', function(self, delta)
 	if delta > 0 then MinimapZoomIn:Click() elseif delta < 0 then MinimapZoomOut:Click() end
 end)
 
---[[local m_coord = CreateFrame("Frame", "DuffedUIMinimapCoord", DuffedUIMinimap)
+local m_coord = CreateFrame('Frame', 'DuffedUIMinimapCoord', DuffedUIMinimap)
 m_coord:Size(40, 20)
-m_coord:Point("BOTTOMLEFT", DuffedUIMinimap, "BOTTOMLEFT", 5, -2)
+m_coord:Point('BOTTOMLEFT', DuffedUIMinimap, 'BOTTOMLEFT', 5, -2)
 m_coord:SetFrameLevel(Minimap:GetFrameLevel() + 3)
 m_coord:SetFrameStrata(Minimap:GetFrameStrata())
 
-local m_coord_text = m_coord:CreateFontString("DuffedUIMinimapCoordText", "Overlay")
-m_coord_text:SetFont(C["media"].font, 11, "THINOUTLINE")
-m_coord_text:Point("Center", -1, 0)
-m_coord_text:SetText(" ")
+local m_coord_text = m_coord:CreateFontString('DuffedUIMinimapCoordText', 'Overlay')
+m_coord_text:SetFont(C['media'].font, 11, 'THINOUTLINE')
+m_coord_text:Point('Center', -1, 0)
+m_coord_text:SetText(' ')
 
 local int = 0
-m_coord:HookScript("OnUpdate", function(self, elapsed)
+m_coord:HookScript('OnUpdate', function(self, elapsed)
 	int = int + 1
 	if int >= 3 then
-		if not GetPlayerMapPosition("player") then
-			m_coord_text:SetText(" ")
+		local UnitMap = C_Map.GetBestMapForUnit('player')
+		local x, y = 0, 0
+
+		if not C_Map.GetPlayerMapPosition(UnitMap, 'player') then
+			coords.PlayerText:SetText('x, x')
 			return
 		end
-
-		local x, y = GetPlayerMapPosition("player")
+		
+		if UnitMap then
+			local GetPlayerMapPosition = C_Map.GetPlayerMapPosition(UnitMap, 'player')
+			if GetPlayerMapPosition then x, y = C_Map.GetPlayerMapPosition(UnitMap, 'player'):GetXY() end
+		end		
 		x = math.floor(100 * x)
 		y = math.floor(100 * y)
-		if x ~= 0 and y ~= 0 then m_coord_text:SetText(x .. " - " .. y) else m_coord_text:SetText(" ") end
+		if x ~= 0 and y ~= 0 then m_coord_text:SetText(x .. ' - ' .. y) else m_coord_text:SetText(' ') end
 		int = 0
 	end
-end)]]--
+end)
