95a95ac65db799dcab7dd0023ff52d7a14e0a92e
Christian Hann <c-hann@users.noreply.github.com>
2018-07-24 18:28:42 +0200

    Readded DuffedUI to ProjektAzilroka

--- ProjectAzilroka/Init.lua
+++ ProjectAzilroka/Init.lua
@@ -64,6 +64,7 @@ PA.SLE = PA:IsAddOnEnabled('ElvUI_SLE', PA.MyName)
 PA.CUI = PA:IsAddOnEnabled('ElvUI_ChaoticUI', PA.MyName)
 PA.Tukui = PA:IsAddOnEnabled('Tukui', PA.MyName)
 PA.AzilUI = PA:IsAddOnEnabled('AzilUI', PA.MyName)
+PA.DuffedUI = PA:IsAddOnEnabled('DuffedUI')
 
 PA.Classes = {}
 
