ea612b55579240db9b715f490d12e043270f70f2
liquidbase <liquid.base.x@gmail.com>
2018-08-05 10:08:45 +0200

    Combined some files to onw and removed the old one

--- DuffedUI/modules/misc/durability.lua
+++
@@ -1,8 +0,0 @@
-local D, C, L = unpack(select(2, ...))
-
-hooksecurefunc(DurabilityFrame, "SetPoint", function(self, _, parent)
-	if (parent == "MinimapCluster") or (parent == _G["MinimapCluster"]) then
-		self:ClearAllPoints()
-		self:Point("BOTTOM", UIParent, "BOTTOM", 0, 200)
-	end
-end)
