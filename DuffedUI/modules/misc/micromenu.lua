acf001fddde6ebcf671fe6d19776da7e8f1b4c35
Christian Hann <c-hann@users.noreply.github.com>
2019-03-14 16:23:55 +0100

    Removed white spaces and SetCheckedTexture in bag script

--- DuffedUI/modules/misc/micromenu.lua
+++ DuffedUI/modules/misc/micromenu.lua
@@ -7,21 +7,21 @@ D['MicroMenu'] = {
 	{text = SPELLBOOK_ABILITIES_BUTTON, func = function() ToggleFrame(SpellBookFrame) end, notCheckable = true},
 	{text = TALENTS_BUTTON, func = function() if not PlayerTalentFrame then TalentFrame_LoadUI() end ShowUIPanel(PlayerTalentFrame) end, notCheckable = true},
 	{text = ACHIEVEMENT_BUTTON, func = function() ToggleAchievementFrame() end, notCheckable = true},
-	{text = MOUNTS,	func = function() ToggleCollectionsJournal(1) end, notCheckable = true},    
-    {text = PETS, func = function() ToggleCollectionsJournal(2) end, notCheckable = true},    
-    {text = TOY_BOX, func = function() ToggleCollectionsJournal(3) end, notCheckable = true},    
+	{text = MOUNTS,	func = function() ToggleCollectionsJournal(1) end, notCheckable = true},
+    {text = PETS, func = function() ToggleCollectionsJournal(2) end, notCheckable = true},
+    {text = TOY_BOX, func = function() ToggleCollectionsJournal(3) end, notCheckable = true},
     {text = HEIRLOOMS, func = function() ToggleCollectionsJournal(4) end, notCheckable = true},
 	{text = WARDROBE, func = function() ToggleCollectionsJournal(5) end, notCheckable = true},
 	{text = SOCIAL_BUTTON, func = function() ToggleFriendsFrame() end, notCheckable = true},
 	{text = COMPACT_UNIT_FRAME_PROFILE_AUTOACTIVATEPVE..' / '..COMPACT_UNIT_FRAME_PROFILE_AUTOACTIVATEPVP, func = function() PVEFrame_ToggleFrame() end, notCheckable = true},
 	{text = ACHIEVEMENTS_GUILD_TAB,
-	func = function() 
-		if IsInGuild() then 
-			if not GuildFrame then GuildFrame_LoadUI() end 
-			GuildFrame_Toggle() 
+	func = function()
+		if IsInGuild() then
+			if not GuildFrame then GuildFrame_LoadUI() end
+			GuildFrame_Toggle()
 		else 
-			if not LookingForGuildFrame then LookingForGuildFrame_LoadUI() end 
-			LookingForGuildFrame_Toggle() 
+			if not LookingForGuildFrame then LookingForGuildFrame_LoadUI() end
+			LookingForGuildFrame_Toggle()
 		end
 	end, notCheckable = true},
 	{text = RAID, func = function() ToggleFriendsFrame(4) end, notCheckable = true},
