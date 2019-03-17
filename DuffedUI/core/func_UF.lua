d312212a58c93d85e266444f3f1d7c091721b453
Rav <ravenrnt@gmx.de>
2019-03-17 20:04:30 +0100

    Update func_UF.lua
    New buffids

--- DuffedUI/core/func_UF.lua
+++ DuffedUI/core/func_UF.lua
@@ -578,30 +578,42 @@ end
 --Raidbuffs & -debuffs
 D['Buffids'] = {
 	PRIEST = {
-		{41635, 'BOTTOMRIGHT', {0.2, 0.7, 0.2}},             -- Prayer of Mending
-		{139, 'BOTTOMLEFT', {0.4, 0.7, 0.2}},                -- Renew
-		{17, 'TOPLEFT', {0.81, 0.85, 0.1}, true},            -- Power Word: Shield
+		{194384, "TOPRIGHT", {1, 0, 0.75}}, -- Atonement
+		{214206, "TOPRIGHT", {1, 0, 0.75}}, -- Atonement PvP
+		{41635, "BOTTOMRIGHT", {0.2, 0.7, 0.2}}, -- Prayer of Mending
+		{193065, "BOTTOMRIGHT", {0.54, 0.21, 0.78}}, -- Masochism
+		{139, "BOTTOMLEFT", {0.4, 0.7, 0.2}}, -- Renew
+		{6788, "BOTTOMLEFT", {0.89, 0.1, 0.1}}, -- Weakened Soul
+		{17, "TOPLEFT", {0.81, 0.85, 0.1}, true}, -- Power Word: Shield
+		{47788, "LEFT", {221 / 255, 117 / 255, 0}, true}, -- Guardian Spirit
+		{33206, "LEFT", {227 / 255, 23 / 255, 13 / 255}, true} -- Pain Suppression
 	},
 
 	DRUID = {
-		{774, 'TOPLEFT', {0.8, 0.4, 0.8}},                   -- Rejuvenation
-		{155777, 'LEFT', {0.8, 0.4, 0.8}},                   -- Germination
-		{8936, 'TOPRIGHT', {0.2, 0.8, 0.2}},                 -- Regrowth
-		{33763, 'BOTTOMLEFT', {0.4, 0.8, 0.2}},              -- Lifebloom
-		{48438, 'BOTTOMRIGHT', {0.8, 0.4, 0}},               -- Wild Growth
+		{774, "TOPRIGHT", {0.8, 0.4, 0.8}}, -- Rejuvenation
+		{155777, "RIGHT", {0.8, 0.4, 0.8}}, -- Germination
+		{8936, "BOTTOMLEFT", {0.2, 0.8, 0.2}}, -- Regrowth
+		{33763, "TOPLEFT", {0.4, 0.8, 0.2}}, -- Lifebloom
+		{48438, "BOTTOMRIGHT", {0.8, 0.4, 0}}, -- Wild Growth
+		{207386, "TOP", {0.4, 0.2, 0.8}}, -- Spring Blossoms
+		{102351, "LEFT", {0.2, 0.8, 0.8}}, -- Cenarion Ward (Initial Buff)
+		{102352, "LEFT", {0.2, 0.8, 0.8}}, -- Cenarion Ward (HoT)
+		{200389, "BOTTOM", {1, 1, 0.4}} -- Cultivation
 	},
 
 	PALADIN = {
-		{53563, 'TOPLEFT', {0.7, 0.3, 0.7}},	             -- Beacon of Light
-		{156910, 'TOPRIGHT', {0.7, 0.3, 0.7}},	             -- Beacon of Faith
-		{200025, 'TOPRIGHT', {0.7, 0.3, 0.7}},				 -- Beacon of Virtue
-		{1022, 'BOTTOMRIGHT', {0.2, 0.2, 1}, true}, 	     -- Hand of Protection
-		{1044, 'BOTTOMRIGHT', {0.89, 0.45, 0}, true},	     -- Hand of Freedom
-		{6940, 'BOTTOMRIGHT', {0.89, 0.1, 0.1}, true},	     -- Hand of Sacrifice
+		{53563, "TOPRIGHT", {0.7, 0.3, 0.7}}, -- Beacon of Light
+		{156910, "TOPRIGHT", {0.7, 0.3, 0.7}}, -- Beacon of Faith
+		{200025, "TOPRIGHT", {0.7, 0.3, 0.7}}, -- Beacon of Virtue
+		{1022, "BOTTOMRIGHT", {0.2, 0.2, 1}, true}, -- Hand of Protection
+		{1044, "BOTTOMRIGHT", {0.89, 0.45, 0}, true}, -- Hand of Freedom
+		{6940, "BOTTOMRIGHT", {0.89, 0.1, 0.1}, true}, -- Hand of Sacrifice
+		{223306, "BOTTOMLEFT", {0.7, 0.7, 0.3}, true}, -- Bestow Faith
 	},
 
 	SHAMAN = {
-		{61295, 'TOPLEFT', {0.7, 0.3, 0.7}},                 -- Riptide
+		{61295, "TOPRIGHT", {0.7, 0.3, 0.7}}, -- Riptide
+		{974, "BOTTOMRIGHT", {0.2, 0.2, 1}} -- Earth Shield
 	},
 
 	MONK = {
@@ -609,6 +621,23 @@ D['Buffids'] = {
 		{116849, 'TOPRIGHT', {0.2, 0.8, 0.2}},	             -- Life Cocoon
 		{124682, 'BOTTOMLEFT', {0.4, 0.8, 0.2}},             -- Enveloping Mist
 		{124081, 'BOTTOMRIGHT', {0.7, 0.4, 0}},              -- Zen Sphere
+	},
+	
+	ROGUE = {
+		{57934, "TOPRIGHT", {227 / 255, 23 / 255, 13 / 255}} -- Tricks of the Trade
+	},
+
+	WARRIOR = {
+		{114030, "TOPLEFT", {0.2, 0.2, 1}}, -- Vigilance
+		{147833, "TOPRIGHT", {227 / 255, 23 / 255, 13 / 255}} -- Intervene
+	},
+	
+	PET = {
+		-- Warlock Pets
+		{193396, "TOPRIGHT", {0.6, 0.2, 0.8}, true}, -- Demonic Empowerment
+		-- Hunter Pets
+		{19615, "TOPLEFT", {227 / 255, 23 / 255, 13 / 255}, true}, -- Frenzy
+		{136, "TOPRIGHT", {0.2, 0.8, 0.2}, true} -- Mend Pet
 	},
 
 	ALL = {
