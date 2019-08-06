7c4a383f1de1e2c40c3d87fcf5c781fa8e21ede8
Rav <ravenrnt@gmx.de>
2019-08-06 19:09:28 +0200

    Update gold.lua
    - Add Seafarer's Dubloon

--- DuffedUI/modules/datatext/gold.lua
+++ DuffedUI/modules/datatext/gold.lua
@@ -233,6 +233,7 @@ Stat:SetScript('OnEnter', function(self)
 		D['Currency'](1149, false, true)
 		D['Currency'](1154, false, true)
 		D['Currency'](1275)
+		D['Currency'](1710)
 		D['Currency'](1721) -- Patch 8.2
 	end
 
